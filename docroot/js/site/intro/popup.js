b74ec529f8b5e0c1cc298b7fa6c67a8164641992
melvin.ma <melvin.ma@TTGSSDV0VRHCM01.TTGTPMG.NET>
2011-03-07 18:31:12 +0000

    adjusted the popup window size

--- docroot/js/site/intro/popup.js
+++ docroot/js/site/intro/popup.js
@@ -1,4 +1,4 @@
 	function popUrl(url) {
-		var load = window.open(url,'memberVideoWizard','width=700 height=400,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0'); 
+		var load = window.open(url,'memberVideoWizard','width=700 height=350,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0'); 
 		return false; 
 	}
