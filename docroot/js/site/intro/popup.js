65d516a0ba9ed40efc42298386284a364c1ed5d4
H588882 <h588882@TTGSSDV0VRHCM01.TTGTPMG.NET>
2012-04-17 12:27:04 +0000

    changed the popup size of the setup wizard popup

--- docroot/js/site/intro/popup.js
+++ docroot/js/site/intro/popup.js
@@ -1,4 +1,5 @@
 	function popUrl(url) {
-		var load = window.open(url,'memberVideoWizard','width=700,height=350,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0'); 
+		var load = window.open(url,'memberVideoWizard','width=500,height=154,status=0,toolbar=0,location=no,menubar=0,resizable=0,scrollbars=0,titlebar=no,statusbar=no'); 
+		
 		return false; 
 	}
