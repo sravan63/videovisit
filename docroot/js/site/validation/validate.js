5bd0d3c4091d86fafbe2af7a8d66e4c46e28326d
W992736 <w992736@TTGSSDV0VRHCM01.TTGTPMG.NET>
2012-11-30 19:29:43 +0000

    Fixed an issue when with validations- Initially Not checking if all the validations are successful or not before returning the result

--- docroot/js/site/validation/validate.js
+++ docroot/js/site/validation/validate.js
@@ -3,9 +3,47 @@ var METHODNAME_IS_ALPHA_NUMERIC = 1;
 var METHODNAME_IS_VALUE_BETWEEN_MIN_AND_MAX = 2
 var METHODNAME_IS_MAX_LENGTH = 3
 
+
+/**
+ * Data should be passed in the following JSON format. See example below
+ * last_name - Id of the field to be validated
+ * Each array element will have the 
+ *			Required parameters: 
+ *			METHOD_NAME- an int value which determines which method to be called, 
+ *			ERROR_MESSAGE - In case the validation fails
+ *			ERROR_ID - Error Id that will display the error
+ *			HIGHLIGHT_PARENT_WHEN_ERROR - Input that needs to be highlighted when the error occurs. For e.g if text field has error then highlight the text field
+ *			PARAM_VALUE - value which needs to be validated.
+ *		 	Optional Parameters - Depending on the methods to be called additional parameters may be passed.
+ * var validationObj = 
+ *		{
+ *			"last_name" : [
+ *				{
+ *					"METHOD_NAME" : METHODNAME_IS_REQUIRED,
+ *					"PARAM_VALUE" : $("#last_name").val(),
+ *					"METHOD_ERROR_MESSAGE" : "Please enter your last name.",
+ *					"ERROR_ID" : "lastNameErrorId",
+ *					"HIGHLIGHT_PARENT_WHEN_ERROR": true
+ *					
+ *					
+ *				},
+ *				{
+ *					"METHOD_NAME" : METHODNAME_IS_ALPHA_NUMERIC,
+ *					"PARAM_VALUE" : $("#last_name").val(),
+ *					"ERROR_MESSAGE" : "Please enter your last name.",
+ *					"ERROR_ID" : "lastNameErrorId",
+ *					"HIGHLIGHT_PARENT_WHEN_ERROR": true 
+ *
+ *				}
+ *			]
+ * @param validationObj
+ * @returns {Boolean}
+ */
 function validate(validationObj){
 	
 	var isValid = false;
+	// returns true is all the validations are successful
+	var isAllValid = true;
 	// Iterate over outer collection which is a set of html input Ids. Each Id will have an array of validations that needs to be performed.
 	
 	$.each(validationObj, function(key, arrValidationItems) {
@@ -32,26 +70,31 @@ function validate(validationObj){
 	    	
 	    	switch(methodName){
 	    		case METHODNAME_IS_REQUIRED: 
-	    			
 	    			isValid = isRequired(paramValue);
+	    			isAllValid = isAllValid && isValid;
 	    			break;
 	    			
 	    		case METHODNAME_IS_ALPHA_NUMERIC: 
 	    			isValid = isAlphaNumeric(paramValue);
+	    			isAllValid = isAllValid && isValid;
+	    			
 	    			break;
 	    		case METHODNAME_IS_VALUE_BETWEEN_MIN_AND_MAX: 
 	    			var min = methodObj.PARAM_MIN_VALUE;
 	    			var max = methodObj.PARAM_MAX_VALUE;
 	    			isValid = isValueBetweenMinMax(paramValue, min, max);
+	    			isAllValid = isAllValid && isValid;
 	    			break;
 	    		case METHODNAME_IS_MAX_LENGTH: 
 	    			var max = methodObj.PARAM_MAX_VALUE;
 	    			
 	    			isValid = isMaxLength(paramValue, max);
+	    			isAllValid = isAllValid && isValid;
 	    			break;
 	    				
 	    		default:
 	    			isValid = false;
+	    			isAllValid = isAllValid && isValid;
 	    			break;
 	    	}
 	    	
@@ -65,8 +108,8 @@ function validate(validationObj){
 	});
 	
 	
-	
-	return isValid;
+
+	return isAllValid ;
 }
 
 /**
