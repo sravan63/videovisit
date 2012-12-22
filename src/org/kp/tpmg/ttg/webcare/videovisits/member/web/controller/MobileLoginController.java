df72fa76c2c043eef9078d626a6d16f8f288abe1
W992736 <w992736@TTGSSDV0VRHCM01.TTGTPMG.NET>
2012-12-22 00:54:00 +0000

    Added this controller for use case logout > Sign on for mobile For mobile version, after logoff since we are directly sending  a json request for login, since there is no WebAppContext, the DataSessionFilter redirects to error page. WebAppContext is created only in AppRootController which gets called only for .htm request and hence we put this special controller in place. Also excluded the mobilelogin.json from the web.xml DataSessionFilter as we do not want to throw an error for this case

---
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/MobileLoginController.java
@@ -0,0 +1,60 @@
+package org.kp.tpmg.ttg.webcare.videovisits.member.web.controller;
+
+import java.util.ResourceBundle;
+
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.command.MeetingCommand;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.command.WebAppContextCommand;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.context.WebAppContext;
+
+import javax.servlet.http.*;
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+
+public class MobileLoginController extends SimplePageController {
+	public static Logger logger = Logger.getLogger(MobileLoginController.class);
+	private static String JSONMAPPING = "jsonData";
+
+	public ModelAndView handlePageRequest(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		
+		logger.info("Entering MobileLoginController:handlePageRequest");
+		
+		// Check if the context is active. This use case will be executed if the login controller is called for logoff > sign on
+		WebAppContext ctx = WebAppContext.getWebAppContext(request);
+		if (ctx == null){
+			ctx = WebAppContextCommand.createContext(request, "0");
+			WebAppContext.setWebAppContext(request, ctx);
+			
+			ResourceBundle rbInfo = ResourceBundle.getBundle("configuration");
+			String megaMeetingURL = rbInfo.getString ("MEGA_MEETING_URL");	
+			String megaMeetingMobileURL = rbInfo.getString ("MEGA_MEETING_MOBILE_URL");	
+			
+			ctx.setMegaMeetingURL(megaMeetingURL);	
+			ctx.setMegaMeetingMobileURL(megaMeetingMobileURL);
+
+		}
+		
+		
+		String data = null;
+		
+
+		try
+		{
+			//verify member from web service and perform meeting analysis.
+			 data = MeetingCommand.verifyMember(request, response);
+			 
+			 logger.info("MobileLoginController:handlePageRequest:data:" + data);
+	}
+		catch (Exception e)
+		{
+			// log error
+			logger.error("System Error" + e.getMessage(),e);
+		}
+		//put data into buffer
+		
+		modelAndView.setViewName(JSONMAPPING);
+		modelAndView.addObject("data", data);
+		return (modelAndView);
+	}
+}
+
