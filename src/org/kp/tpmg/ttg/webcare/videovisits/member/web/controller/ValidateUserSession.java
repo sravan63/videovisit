bcdedc331e985acd3fc8f55624820b7f505a1850
W992736 <w992736@TTGSSDV0VRHCM01.TTGTPMG.NET>
2013-01-22 20:08:18 +0000

    US1634- Mobile iOS: Direct user to sign off page after being timed out.

---
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/ValidateUserSession.java
@@ -0,0 +1,66 @@
+package org.kp.tpmg.ttg.webcare.videovisits.member.web.controller;
+
+
+
+import javax.servlet.http.*;
+
+import net.sf.json.JSONObject;
+
+import org.apache.log4j.Logger;
+
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.context.WebAppContext;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * This class is responsible for setting the session timeout for the web application or mobile context
+ * @author arunwagle
+ *
+ */
+public class ValidateUserSession extends SimplePageController {
+
+	public static Logger logger = Logger.getLogger(ValidateUserSession.class);
+	private static String JSONMAPPING = "jsonData";
+
+
+	public ModelAndView handlePageRequest(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) 
+		throws Exception
+	{
+		String data = "success";
+		JSONObject result = new JSONObject();
+		try
+		{
+			HttpSession session = request.getSession(false);
+			
+			result.put("isValidUserSession", false);
+			result.put("success", false);
+			if(session != null){
+				
+				WebAppContext context = WebAppContext.getWebAppContext(request);
+				
+				// session is active
+				if(context != null && context.getMember() != null){
+					result.put("isValidUserSession", true);
+					result.put("success", true);	
+				}
+			}
+			
+			
+		}
+		catch (Exception e)
+		{
+			// log error
+			logger.error("System Error" + e.getMessage(),e);
+		}
+		//put data into buffer
+		
+		data = JSONObject.fromObject(result).toString();
+		logger.info("ValidateUserSession-handleRequest-data="+data);
+		modelAndView.setViewName(JSONMAPPING);
+		modelAndView.addObject("data", data);
+		return (modelAndView);
+
+	}
+
+	
+
+}
