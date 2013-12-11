b9c649207eaacfb66e2483d4bfa3a8b180654e6f
C212671 <c212671@TTGSSDV0VRHCM01.TTGTPMG.NET>
2013-12-11 22:35:53 +0000

    BF: Guest Vidyo Player was loading in a small size

--- docroot/js/site/videovisit/videoVisit.js
+++ docroot/js/site/videovisit/videoVisit.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
 
 	var finalHeight = $(window).height();
 	$('#joinNowIframe').css({"height": finalHeight*0.90});
+	$('#joinNowIframeGuest').css({"height": finalHeight*0.90});
 });
