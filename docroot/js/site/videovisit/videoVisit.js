4cb6e953416822d60ff30e377361a4c590facd79
C212671 <c212671@TTGSSDV0VRHCM01.TTGTPMG.NET>
2014-06-06 22:54:55 +0000

    US4421: Access Control Bar consistently US4438: Info Panel

--- docroot/js/site/videovisit/videoVisit.js
+++ docroot/js/site/videovisit/videoVisit.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
 
+	var finalWidth = $(window).width();
+	console.log("finalWidth: " +finalWidth);
 	var finalHeight = $(window).height();
-	$('#joinNowIframe').css({"height": finalHeight*0.90});
-	$('#joinNowIframeGuest').css({"height": finalHeight*0.90});
+	console.log("finalHeight: " +finalHeight);
+	//$('#joinNowIframe').css({"height": finalHeight*0.90});
+	//$('#joinNowIframeGuest').css({"height": finalHeight*0.90});
+
+	var calculatedWidth = finalWidth - 210;
+	console.log("calculatedWidth: "+calculatedWidth);
+	var calculatedHeight = finalHeight;
+	console.log("calculatedHeight: "+calculatedHeight);
+
+	$("#video-main").width(calculatedWidth);
+	$("#video-main").height(calculatedHeight);
+	$("#video-sidebar").height(calculatedHeight);
+
+	var calculatedWidthPluginContainer = calculatedWidth-80;
+	console.log("calculatedWidthPluginContainer: " +calculatedWidthPluginContainer);
+	$("#pluginContainer").width(calculatedWidthPluginContainer);
+	$("#pluginContainer").height(calculatedHeight);
+	$("#btnContainer").height(calculatedHeight);
 });
