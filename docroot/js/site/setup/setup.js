e66a4d0153e741f7f7d920eb290b66a90c3581ba
A535480 <a535480@TTGSSDV0VRHCM01.TTGTPMG.NET>
2014-07-28 15:53:19 +0000

    TA3114,TA3115 - New Setup wizard backend flow

--- docroot/js/site/setup/setup.js
+++ docroot/js/site/setup/setup.js
@@ -1,29 +1,34 @@
 $(document).ready(function() {
 
-	/*var windowHeight = $(window).height();
-	var windowWidth = $(window).width();
-	
-	var vvHeaderHeight = $("#vvHeader").outerHeight();
-	var videoSidebarWidth = $("#video-sidebar").outerWidth();
-	var calculatedHeight = windowHeight - vvHeaderHeight;
-	var calculatedWidth = windowWidth - videoSidebarWidth;
-	
-	$("#video-main").width(calculatedWidth);
-	$("#video-main").height(calculatedHeight);
-	$("#video-sidebar").height(calculatedHeight);
-
-	var btnContainerWidth = $("#btnContainer").outerWidth();
-	var calculatedWidthPluginContainer = calculatedWidth - btnContainerWidth;
-
-	$("#pluginContainer").width(calculatedWidthPluginContainer);
-	$("#pluginContainer").height(calculatedHeight);
-	$("#btnContainer").height(calculatedHeight);	*/
-
-	//alert("inCallContainer: " +$("#inCallContainer").width());
-	//alert("inCallPluginAndControlsWrap: " +$("#inCallPluginAndControlsWrap").width());
-	//alert("pluginContainer: " +$("#pluginContainer").width());
-
 	var configContainerWidth = $("#setupCameraButtonToggleConfig").width();
 	$(".setupAccessoryConfigContainer").innerWidth(configContainerWidth);
+
+});
+
+var VideoVisitSetupWizard =
+{
+	terminateMeeting: function(meetingId, vendorConfId)
+	{
+		postPara = {meetingId: meetingId, vendorConfId: vendorConfId};
+        console.log("Before calling ajax terminateSetupWizardMeeting - postParams: " + postPara.meetingId + ", " + postPara.vendorConfId + " - Ajax url: " + VIDEO_VISITS.Path.visit.terminateSetupWizardMeeting);
+        $.ajax({
+	        type: "POST",
+	        url: VIDEO_VISITS.Path.visit.terminateSetupWizardMeeting,
+	        cache: false,
+		    async: false,
+	        data: postPara, 
+	        success: function(returndata) {
+	        	console.log("Setupwizard Meeting terminated successfully - returndata: " + returndata);
+			},
+	        error: function() {
+	        	console.log("Terminate setup wizard meeting failed");
+			}
+	    });
+  	}
+}
+
+$(window).on("beforeunload", function() { 
+    console.log("Inside before unload"); 
+     VideoVisitSetupWizard.terminateMeeting($("#meetingId").val(), $("#vendorConfId").val());
 
 });
