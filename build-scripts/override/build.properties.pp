195f77fe28e3634f84f153eb3cb2eadb353d36d5
A535480 <a535480@TTGSSDV0VRHCM01.TTGTPMG.NET>
2014-08-15 16:12:01 +0000

    Napa Merge from 5.3 compatible with wsas 5.2.1.

--- build-scripts/override/build.properties.pp
+++ build-scripts/override/build.properties.pp
@@ -2,12 +2,12 @@ webapp.name=videovisit
 exp.war.target.dir=/usr/local/tomcat5080/webapps/
 war.target.dir=/usr/local/tomcat5080/webapps/
 videovisit_member_package_name=org.kp.tpmg.webservice.client.videovisit.member
-##videovisit_member_endpoint=http\://ttg-dv-esb-1.har.ca.kp.org\:29763/services/VideoVisitMemberServices?wsdl2
-videovisit_member_endpoint=http\://ttg-qa-esb-1.har.ca.kp.org\:8008/services/VideoVisitMemberServices?wsdl2
-##videovisit_member_endpoint=http\://ttg-pp-esb-7.har.ca.kp.org\:48008/services/VideoVisitMemberServices?wsdl2
+videovisit_member_endpoint=http\://TTG-QA-WSO2-1.har.ca.kp.org\:48008/services/VideoVisitMemberServices?wsdl
+##videovisit_member_endpoint=http\://ttgnapp1vrhas01.ttgtpmg.net\:29763/services/VideoVisitMemberServices?wsdl2
 webapp_location=../vvm
 buildtool_location=../esb-client-tools
 videovisit_member_jarname=videovisit-memberservice-client.jar
 build.env=pp
+wsas_home=wso2as-5.2.1
 
 
