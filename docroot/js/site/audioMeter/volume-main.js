0041478cfc6ffd9d311073c12cb7415ab9ab5c5f
C212671 <c212671@TTGSSDV0VRHCM01.TTGTPMG.NET>
2014-09-15 23:08:01 +0000

    US4427: Microphone Volume Level Meter

--- docroot/js/site/audioMeter/volume-main.js
+++ docroot/js/site/audioMeter/volume-main.js
@@ -58,8 +58,17 @@ window.onload = function() {
 	        alert('getUserMedia threw exception :' + e);
 	    }
 	}
+	else if(browserInfoForAM.isIE == true){
+		console.log("IE Browser - doesn't support Audio Meter");
+		$("#mic-demo").css('color', 'black');
+		$("#mic-demo").html("<span style='text-align:left; padding:10px; width:auto;'> To adjust mic volume: <ul style='margin:0;'> <li>Go to Control Panel > Hardware and Sound.</li><li>Under Sound, go to <span style='font-weight:bold; display:inline;'>Manage audio</span> devices.</li><li>Click <span style='font-weight:bold; display:inline;'>Recording</span> tab</li><li>Click <span style='font-weight:bold; display:inline;'>Properties</span> button.</li><li>Click <span style='font-weight:bold; display:inline;'>Levels</span> tab</li> </ul> </span>");
+		$("#mic-instructions").html(" ");
+	}
 	else{
-		console.log("Browser not supported for Audio Meter");
+		console.log("Unknown Browser - doesn't support Audio Meter");
+		$("#mic-demo").css('color', 'black');
+		$("#mic-demo").html(" ");
+		$("#mic-instructions").html(" ");
 	}
 
 };
@@ -95,7 +104,7 @@ function drawLoop( time ) {
         canvasContext.fillStyle = "green";
 
     // draw a bar based on the current volume
-    canvasContext.fillRect(0, 0, meter.volume*WIDTH*7.4, HEIGHT);
+    canvasContext.fillRect(0, 0, meter.volume*WIDTH*2.4, HEIGHT);
 
     // set up the next visual callback
     rafID = window.requestAnimationFrame( drawLoop );
