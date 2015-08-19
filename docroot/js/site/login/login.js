9303be2cca7519d13ee11c8886a522a77d2b058d
C212671 <c212671@TTGSSDV0VRHCM01.TTGTPMG.NET>
2015-08-19 22:15:32 +0000

    US7459: Revised Desktop Login page

--- docroot/js/site/login/login.js
+++ docroot/js/site/login/login.js
@@ -1,178 +1,180 @@
 $(document).ready(function() {
-
-    $loginForm = $("#loginForm");
-    
-    $("#birth_date").mask("99/9999",{placeholder:"mm/yyyy"});
-    $(":text").on('keyup', function(){
-        //alert("disabled");
-        if($('#last_name').val() != "" && $('#mrn').val() != "" && $('#birth_date').val() != ""){
+	$("#birth_date").mask("99/9999",{placeholder:"mm/yyyy"});
+	
+	//Disable the Login button unless all the fields are entered
+	$(":input").on('keyup', function(){
+        if($('#last_name').val() != "" && $('#mrn').val() != "" && ($('#birth_date').val() != "mm/yyyy" && $('#birth_date').val() != "")){
             $('#login').removeAttr('disabled');
+            $('#login').css('cursor', 'pointer');
+            $('input#login').css('opacity', '1.0');
         }
         else{
             $('#login').attr('disabled', true);
+            $('#login').css('cursor', 'default');
+            $('input#login').css('opacity', '0.5');
         }
     });
-
-    $('#login').click(function() {
-    	// show the dialog 
-    	
+	
+	$("form :input").focus(function() {
+		// clear all errors
+		clearAllErrorFields();
+	});
+	
+	$('#login').click(function(e) {
+		e.preventDefault();
 		
-        $loginForm.validate({
-            // Where do we want errors to appear?
-            errorLabelContainer: $('p.error'),
-            // Wrap error messages in a list item
-            wrapper: 'li',
-            // Validation rules for each field in the form
-            rules:  {
-                last_name: {
-                    required: true,
-                    letterswithbasicpunc: true,
-                    minlength: 2
-                },
-                mrn: {
-                    required: true,
-                    digits: true,
-                    minlength: 1,
-                    maxlength: 8
-                },
-                /*birth_month: {
-                    required: true,
-                    digits: true,
-                    minlength: 1,
-                    maxlength: 2
-                },
-                birth_year: {
-                    required: true,
-                    digits: true,
-                    minlength: 4,
-                    maxlength: 4
-                },
-                birth_day: {
-                    required: true,
-                    digits: true,
-                    minlength: 1,
-                    maxlength: 2
-                },
-                captcha: {
-                    required: true,
-                    minlength: 5,
-                    maxlength: 5
-                }
-                */
-                birth_date: {
-                	required: true,
-                    minlength: 7,
-                    maxlength: 7
-                }
-            },
-            // Error messages for each field in the form (corresponds to Rules)
-            messages: {
-                last_name: {
-                    required: "Please enter your last name.",
-                    letterswithbasicpunc: "Please enter a valid name."
-                },
-                mrn: {
-                    required: "Please enter your Medical Record Number.",
-                    digits: "Please enter a valid Medical Record Number.",
-                    minlength: "Please enter a valid Medical Record Number.",
-                    maxlength: "Please enter a valid Medical Record Number."
-                },
-                birth_date: {
-                    required: "Please enter your Birth Date.",
-                    maxlength: "You cannot have more than 7 numbers for your Birth Date."
-                }
-            }
-        }); //End validation
+		if ( $("#mrn").val().length > 0)
+		{
+			if ( $("#mrn").val().length < 8 )
+	        {
+	        	while ( $("#mrn").val().length < 8 )
+	        	{
+	        		$("#mrn").val('0' + $("#mrn").val());
+	        	}
+	        }
+        }
+		
+		// if client side validation successful
+		if(isLoginValidationSuccess()){
+			loginSubmit();
+		}
+	});
+});
 
-        if ($loginForm.valid()) {
+function isLoginValidationSuccess(){
+	var birth_month = $("#birth_date").val().split("/")[0];
+    var birth_year = $("#birth_date").val().split("/")[1];
+	
+	var validationObj =
+		{
+			"last_name" : [
+				{
+					"METHOD_NAME" : METHODNAME_IS_ALPHA_NUMERIC,
+					"PARAM_VALUE" : $("#last_name").val(),
+					"ERROR_MESSAGE" : "Only Letters, Hyphens or Apostrophes allowed",
+					"ERROR_ID" : "lastNameErrorId",
+					"HIGHLIGHT_PARENT_WHEN_ERROR": false
+				}
+			],
+			"mrn"	:[
+				{
+					"METHOD_NAME" : METHODNAME_IS_VALUE_BETWEEN_MIN_AND_MAX,
+					"PARAM_VALUE" : $("#mrn").val(),
+					"PARAM_MIN_VALUE" :8,
+					"PARAM_MAX_VALUE" :8,
+					"ERROR_MESSAGE" : "Please enter a valid Medical Record Number",
+					"ERROR_ID" : "mrnErrorId",
+					"HIGHLIGHT_PARENT_WHEN_ERROR": false
+				}
+			],
+			"birth_month"	:[
+	            {
+	            	"METHOD_NAME" : METHODNAME_IS_BIRTHMONTH_VALIDATION,
+					"PARAM_VALUE" : birth_month,
+					"ERROR_MESSAGE" : "Please enter a valid Birth Month",
+					"ERROR_ID" : "monthOfBirthErrorId",
+					"HIGHLIGHT_PARENT_WHEN_ERROR": false
+	            }
+			],
+			"birth_year"	:[
+	            {
+	            	"METHOD_NAME" : METHODNAME_IS_BIRTHYEAR_VALIDATION,
+					"PARAM_VALUE" : birth_year,
+					"ERROR_MESSAGE" : "Please enter a valid Birth Year",
+					"ERROR_ID" : "yearOfBirthErrorId",
+					"HIGHLIGHT_PARENT_WHEN_ERROR": false
+	            }
+			]
+		}
 
-            /* Removed consent requirement, 5/22/12 */
-            
-            $("p.error").html('');
-            
-            // Prepare data from pertinent fields for POSTing
-            var birth_month = $("#birth_date").val().split("/")[0];
-            var birth_year = $("#birth_date").val().split("/")[1];
-            var birth_day = "";
-            
-            // Format birth_month
-            /*var birth_month = $('input[name=birth_month]').val();
-            if (birth_month.length == 1) {
-                birth_month = "0" + birth_month;
-            }
-            // Format birth_day
-            var birth_day = $('input[name=birth_day]').val();
-            if (birth_day.length == 1) {
-                birth_day = "0" + birth_day;
-            }*/
+	var  isValid = validate(validationObj);
 
-            var prepdata = 'last_name=' + $('input[name=last_name]').val() + '&mrn=' + $('input[name=mrn]').val() + '&birth_month=' + birth_month + '&birth_year=' + birth_year + '&birth_day=' + birth_day + '&consentVersion=' + $('input[name=consentVersion]').val();
+	if(isValid){
+		var currentDate = new Date();
+		var currentMonth = currentDate.getMonth() + 1;
+		var currentYear = currentDate.getFullYear();
 
-            $.ajax({
-                type: "POST",
-                url: VIDEO_VISITS.Path.login.ajaxurl,
-                data: prepdata, // alternatively: $(data).serialize() but this adds fields we don't need
-                success: function(returndata) {
-                    returndata = $.trim(returndata);
-                    switch (returndata) {
-                        case "1":
-                        	// show the dialog 
-                        	
-                            window.location.replace("landingready.htm");
-                            break;
+		var selectedMonth = birth_month;
+	    var selectedYear = birth_year;
 
-                        case "2":
-                        	// show the dialog 
-                        	
-                            window.location.replace("landingnone.htm");
-                            break;
+	    //The entered Month of current year should not be in future
+	    if(selectedYear == currentYear){
+    		if(selectedMonth <= currentMonth){
+    			return true;
+    		}
+	    }
+	    else{
+	    	return true;
+	    }
 
-                        case "3":
-                        	// show the dialog 
-                        	
-                            $("p.error").css("display", "inline").append('<li><label>We could not find this patient.  Please try entering the information again.</label></li>');
-                            moveToit("p.error");
-                            $('#last_name').val("");
-                            $('#mrn').val("");
-                            $('#birth_date').val("");
-                            $('#login').attr('disabled', true);
-                            break;
+	    $('#dateOfBirthErrorId').html("Please enter a valid Birth Date").removeClass("hide-me");
+	    return false;
+	}
 
-                        case "4":
-                        	// show the dialog 
-                        	
-                            $("p.error").css("display", "inline").append('<li><label>The code entered did not match. Please try again (you can click the code image to generate a new one if needed).</label></li>');
-                            moveToit("p.error");
-                            $('#last_name').val("");
-                            $('#mrn').val("");
-                            $('#birth_date').val("");
-                            $('#login').attr('disabled', true);
-                            break;
+	return isValid;
+}
 
-                        default:
-                        	// show the dialog 
-                        	
-                            $("p.error").css("display", "inline").append('<li><label>There was an error submitting your login. Please try again later.</label></li>');
-                            moveToit("p.error");
-                            $('#last_name').val("");
-                            $('#mrn').val("");
-                            $('#birth_date').val("");
-                            $('#login').attr('disabled', true);
-                            break;
-                    }
+function loginSubmit(){
+    var birth_month = $("#birth_date").val().split("/")[0];
+    var birth_year = $("#birth_date").val().split("/")[1];
+    var birth_day = "";
 
-                },
-                error: function() {
+	var prepdata = 'last_name=' + $('input[name=last_name]').val() + '&mrn=' + $('input[name=mrn]').val() + '&birth_month=' + birth_month + '&birth_year=' + birth_year + '&birth_day=' + birth_day + '&consentVersion=' + $('input[name=consentVersion]').val();
+
+	$.ajax({
+        type: "POST",
+        url: VIDEO_VISITS.Path.login.ajaxurl,
+        data: prepdata, // alternatively: $(data).serialize() but this adds fields we don't need
+        success: function(returndata) {
+            returndata = $.trim(returndata);
+            switch (returndata) {
+                case "1":
                 	// show the dialog 
                 	
-                    $("p.error").css("display", "inline").append('<li><label>There was an error submitting your login.</label></li>');
-                    moveToit("p.error");
-                }
-            });
-            return false;
-            
-        } 
+                    window.location.replace("landingready.htm");
+                    break;
+
+                case "2":
+                	// show the dialog 
+                	
+                    window.location.replace("landingnone.htm");
+                    break;
+
+                case "3":
+                	// show the dialog 
+                	
+                    $("p#global-Error").removeClass("hide-me").html('We could not find this patient. Please try entering the information again.');
+                    break;
+
+                case "4":
+                	// show the dialog 
+
+                	$("p#global-Error").removeClass("hide-me").html('The code entered did not match. Please try again (you can click the code image to generate a new one if needed).');
+                    break;
+
+                default:
+                	// show the dialog 
+
+                	$("p#global-Error").removeClass("hide-me").html('There was an error submitting your login. Please try again later.');
+                    break;
+            }
+
+        },
+        error: function() {
+        	// show the dialog 
+        	
+            $("p.error").css("display", "inline").append('<li><label>There was an error submitting your login.</label></li>');
+            moveToit("p.error");
+        }
     });
+    return false;
+}
 
-
-});
+function clearAllErrorFields(){
+	$("#lastNameErrorId").html("").addClass("hide-me");
+	$("#mrnErrorId").html("").addClass("hide-me");
+	$("#monthOfBirthErrorId").html("").addClass("hide-me");
+	$("#yearOfBirthErrorId").html("").addClass("hide-me");
+	$("#dateOfBirthErrorId").html("").addClass("hide-me");
+	$("#global-Error").html("").addClass("hide-me");
+}
