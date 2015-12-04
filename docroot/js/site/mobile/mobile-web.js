7a8226be97c9c1aaceb74d5b83c8d47d52e35c1f
X788998 <x788998@TTGSSDV0VRHCM01.TTGTPMG.NET>
2015-12-04 19:37:42 +0000

    Fix for Spinner disappearing before the video call changing timeout from 10000ms to 1500ms

--- docroot/js/site/mobile/mobile-web.js
+++ docroot/js/site/mobile/mobile-web.js
@@ -1156,7 +1156,7 @@ function launchVideoVisitMember(data){
 					window.location.replace(url);
 				    setTimeout(function(){
 				    	$("#layover").hide();
-				    }, 10000);
+				    }, 1500);
 				    
 				}else{
 					openTab(url);
