afcfac21efdb632c0b876553fdd5144826a75e36
C212671 <c212671@TTGSSDV0VRHCM01.TTGTPMG.NET>
2016-01-25 18:51:51 +0000

    US8642 / US9590 : UI of Installation Process for all browsers

--- docroot/js/site/videovisit/videoVisit.js
+++ docroot/js/site/videovisit/videoVisit.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
 	$("#btnContainer").height(calculatedHeight);
 	
 	$("#infoWrapper").width(calculatedWidthPluginContainer);
+	$("#setupContents").width(calculatedWidthPluginContainer);
 });
 
 var VideoVisit =
