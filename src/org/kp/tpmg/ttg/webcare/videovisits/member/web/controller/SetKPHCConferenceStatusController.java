c078dff2ec8f7b6d499e43b70c4be0f199d834b4
A535480 <a535480@TTGSSDV0VRHCM01.TTGTPMG.NET>
2016-03-01 17:14:08 +0000

    US9823 - added controller for calling KPHCSetConfStatus V1 service

---
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/SetKPHCConferenceStatusController.java
@@ -0,0 +1,34 @@
+package org.kp.tpmg.ttg.webcare.videovisits.member.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.command.MeetingCommand;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SetKPHCConferenceStatusController extends SimplePageController 
+{
+	public static Logger logger = Logger.getLogger(SetKPHCConferenceStatusController.class);
+	private static String JSONMAPPING = "jsonData";
+
+	public ModelAndView handlePageRequest(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) throws Exception
+	{	
+		logger.info("Entered SetKPHCConferenceStatusController");
+		String data = null;
+		try
+		{
+			 data = MeetingCommand.setKPHCConferenceStatus(request, response);
+		}
+		catch (Exception e)
+		{
+			// log error
+			logger.error("SetKPHCConferenceStatusController -> System Error:" + e.getMessage(),e);
+		}
+		//put data into buffer
+		logger.info("SetKPHCConferenceStatusController -> handleRequest-data="+data);
+		modelAndView.setViewName(JSONMAPPING);
+		modelAndView.addObject("data", data);
+		return (modelAndView);
+	}
+}
