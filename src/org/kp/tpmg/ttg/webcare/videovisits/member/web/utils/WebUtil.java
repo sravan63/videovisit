d79179d358633ef300f01d53597fe0f7d3c7416d
A535480 <a535480@TTGSSDV0VRHCM01.TTGTPMG.NET>
2016-03-03 21:30:19 +0000

    US10345: added utility methods for cookie operations

--- src/org/kp/tpmg/ttg/webcare/videovisits/member/web/utils/WebUtil.java
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/utils/WebUtil.java
@@ -1,9 +1,15 @@
 package org.kp.tpmg.ttg.webcare.videovisits.member.web.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 import java.util.regex.Pattern;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -13,6 +19,7 @@ public class WebUtil {
 	public static Logger logger = Logger.getLogger(WebUtil.class);
 	private static Pattern DOB_PATTERN = Pattern.compile("\\d\\d\\d\\d-\\d[\\d]-\\d[\\d]");
 	private static Pattern DOB_MMYYYY_PATTERN = Pattern.compile("\\d[\\d]/\\d\\d\\d\\d");
+	public static final String SSO_COOKIE_NAME = "ObSSOCookie";
 	
 	public static String getCurrentDateTimeZone()
 	{
@@ -66,5 +73,70 @@ public class WebUtil {
 		}				
 		return ret;
 	}	
+	
+	/**
+	 * Get cookie based on the cookie name
+	 * @param httpRequest
+	 * @param cookieName
+	 * @return
+	 */
+	public static Cookie getCookie(HttpServletRequest httpRequest, String cookieName) 
+	{		
+		if (httpRequest.getCookies() != null && StringUtils.isNotBlank(cookieName))
+		{
+			for (Cookie cookie : httpRequest.getCookies()) 
+			{
+	        	logger.info("getCookie: cookie name="+ cookie.getName());
+	            if (StringUtils.equalsIgnoreCase(cookie.getName(), cookieName)) 
+	            {
+	                return cookie;
+	            }
+			}
+		}
+        return null;
+    }
+	
+	public static void readCookie(HttpServletRequest httpRequest) 
+	{		
+		if (httpRequest.getCookies() != null)
+		{
+			for (Cookie cookie : httpRequest.getCookies()) 
+			{
+	        	logger.info("getCookie: cookie name="+ cookie.getName());
+	        	logger.info("getCookie: cookie value="+ cookie.getValue());
+	        	logger.info("getCookie: cookie domain="+ cookie.getDomain());
+	        	logger.info("getCookie: cookie maxage="+ cookie.getMaxAge());
+	        	logger.info("getCookie: cookie path="+ cookie.getPath());
+	        	logger.info("getCookie: cookie secure="+ cookie.getSecure());
+	        	logger.info("getCookie: cookie version="+ cookie.getVersion());
+	        	logger.info("getCookie: cookie comment="+ cookie.getComment());
+	        	logger.info("getCookie: -------------------------");
+			}
+		}
+        
+    }
+	
+	public static void setCookie(HttpServletResponse response, String cookieName, String cookieValue)
+	{
+		 logger.info("setCookie: cookie name="+ cookieName + ", cookie value=" + cookieValue);
+		 Cookie cookie;
+		try {
+			cookie = new Cookie(cookieName, URLEncoder.encode(cookieValue, "UTF-8"));
+			cookie.setMaxAge(60 * 20);  // 20 minutes 
+	        response.addCookie(cookie);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			logger.warn("setCookie: error while adding a coockie="+ cookieName + ", cookie value=" + cookieValue);
+		}
+         
+	}
+	
+	public static void removeCookie(HttpServletResponse response, String cookieName)
+	{
+		logger.info("removeCookie: cookie name="+ cookieName);
+		Cookie cookie = new Cookie(cookieName, "");
+		cookie.setMaxAge(0); 
+		response.addCookie(cookie);
+	}
 
 }
