6e9ec6a95d4ef0123648088aa45824070140f4f8
A535480 <a535480@TTGSSDV0VRHCM01.TTGTPMG.NET>
2016-03-08 18:48:23 +0000

    US10345: SSO integration with external apps

--- src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/LogoffController.java
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/LogoffController.java
@@ -1,11 +1,27 @@
 package org.kp.tpmg.ttg.webcare.videovisits.member.web.controller;
 
+import java.net.URLDecoder;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.command.MeetingCommand;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.command.WebAppContextCommand;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.context.WebAppContext;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.parser.FaqParser;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.parser.IconPromoParser;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.parser.PromoParser;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.parser.VideoLinkParser;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.parser.faq;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.parser.iconpromo;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.parser.promo;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.parser.videolink;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.service.DeviceDetectionService;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.utils.WebUtil;
 import org.springframework.web.servlet.ModelAndView;
 
 public class LogoffController extends SimplePageController {
@@ -16,15 +32,64 @@ public class LogoffController extends SimplePageController {
 	public ModelAndView handlePageRequest(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) {
 				
 		boolean isWirelessDeviceorTablet = DeviceDetectionService.isWirelessDeviceorTablet(request);
-		logger.info("isWirelessDeviceorTablet = " + isWirelessDeviceorTablet);
-		logger.info("invalidated session Id=" + request.getSession().getId());
+		logger.info("LogoffController -> isWirelessDeviceorTablet = " + isWirelessDeviceorTablet);
+		logger.info("LogoffController -> session Id=" + request.getSession().getId());
 		if(!isWirelessDeviceorTablet)
 		{
-			try {
-				boolean isSSOSignedOff = MeetingCommand.performSSOSignOff(request, response);
-				logger.debug("LogoffController -> isSSOSignedOff=" + isSSOSignedOff);
-			} catch (Exception e) {
-				logger.warn("LogoffController -> error while SSO sign off");
+			
+			WebAppContext ctx = WebAppContext.getWebAppContext(request);
+			if(ctx == null)
+			{
+				logger.info("LogoffController -> context is null");
+				Cookie ssoCookie = WebUtil.getCookie(request, WebUtil.SSO_COOKIE_NAME);
+											
+				if(ssoCookie != null && StringUtils.isNotBlank(ssoCookie.getValue()))
+				{
+					try
+					{	
+						faq f = FaqParser.parse();
+						List<promo> promos = PromoParser.parse();
+						List<iconpromo> iconpromos = IconPromoParser.parse();
+						videolink videoLink = VideoLinkParser.parse();
+						ctx = WebAppContextCommand.createContext(request, "0");
+						WebAppContext.setWebAppContext(request, ctx);
+						ctx.setFaq(f);
+						ctx.setPromo(promos);
+						ctx.setIconPromo(iconpromos);
+						ctx.setVideoLink(videoLink);
+						String ssoCookieVal = URLDecoder.decode(ssoCookie.getValue(), "UTF-8");
+						
+						String responseCode = MeetingCommand.validateKpOrgSSOSession(request, response, ssoCookieVal);
+						if("200".equalsIgnoreCase(responseCode))
+						{
+							logger.info("LogoffController -> sso session token from request cookie valid, navigating to my meetings page");
+							return new ModelAndView("landingready.htm");
+						}
+						else
+						{
+							logger.info("LogoffController -> invalid sso session token, navigating to SSO login page");
+							boolean isSSOSignedOff = MeetingCommand.performSSOSignOff(request, response);
+							logger.info("LogoffController -> isSSOSignedOff=" + isSSOSignedOff);
+							WebAppContext.setWebAppContext(request, null);
+						}									
+					}
+					catch(Exception ex)
+					{
+						
+					}
+				}			
+			}
+			else
+			{
+				logger.info("LogoffController -> context is not null");
+				try
+				{
+					boolean isSSOSignedOff = MeetingCommand.performSSOSignOff(request, response);
+					logger.debug("LogoffController -> isSSOSignedOff=" + isSSOSignedOff);
+				} 
+				catch (Exception e) {
+					logger.warn("LogoffController -> error while SSO sign off");
+				}
 			}
 		}
 		request.getSession().invalidate();
