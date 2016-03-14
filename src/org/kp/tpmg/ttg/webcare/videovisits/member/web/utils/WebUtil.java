07a14168a494f22512f263b1d8dbb4a367e7eb79
A535480 <a535480@TTGSSDV0VRHCM01.TTGTPMG.NET>
2016-03-14 17:58:13 +0000

    US10345: added utility method to get browser details for SSO sign on API

--- src/org/kp/tpmg/ttg/webcare/videovisits/member/web/utils/WebUtil.java
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/utils/WebUtil.java
@@ -155,5 +155,59 @@ public class WebUtil {
 		}
 			
 	}
+	
+	public static String getBrowserDetails(HttpServletRequest httpRequest)
+	{
+		String browser = "";
+		try
+		{
+			String  browserDetails  =   httpRequest.getHeader("User-Agent");
+	        String  userAgent       =   browserDetails;
+	        String  user            =   userAgent.toLowerCase();
+		       
+	        logger.info("getBrowserDetails -> User Agent for the request is===>"+browserDetails);
+	        //===============Browser===========================
+	        if (user.contains("msie"))
+	        {
+	            String substring=userAgent.substring(userAgent.indexOf("MSIE")).split(";")[0];
+	            browser=substring.split(" ")[0].replace("MSIE", "IE")+"-"+substring.split(" ")[1];
+	        } else if (user.contains("safari") && user.contains("version"))
+	        {
+	            browser=(userAgent.substring(userAgent.indexOf("Safari")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+	        } else if ( user.contains("opr") || user.contains("opera"))
+	        {
+	            if(user.contains("opera"))
+	                browser=(userAgent.substring(userAgent.indexOf("Opera")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+	            else if(user.contains("opr"))
+	                browser=((userAgent.substring(userAgent.indexOf("OPR")).split(" ")[0]).replace("/", "-")).replace("OPR", "Opera");
+	        } else if (user.contains("chrome"))
+	        {
+	            browser=(userAgent.substring(userAgent.indexOf("Chrome")).split(" ")[0]).replace("/", "-");
+	        } else if ((user.indexOf("mozilla/7.0") > -1) || (user.indexOf("netscape6") != -1)  || (user.indexOf("mozilla/4.7") != -1) || (user.indexOf("mozilla/4.78") != -1) || (user.indexOf("mozilla/4.08") != -1) || (user.indexOf("mozilla/3") != -1) )
+	        {
+	            //browser=(userAgent.substring(userAgent.indexOf("MSIE")).split(" ")[0]).replace("/", "-");
+	            browser = "Netscape-?";
+	
+	        } else if (user.contains("firefox"))
+	        {
+	            browser=(userAgent.substring(userAgent.indexOf("Firefox")).split(" ")[0]).replace("/", "-");
+	        } else if(user.contains("rv"))
+	        {
+	            browser="IE";
+	        } else
+	        {
+	            browser = "UnKnown, More-Info: "+userAgent;
+	        }	        
+		}
+		catch(Exception ex)
+		{
+			if(browser == null)
+			{
+				browser = "";
+			}
+		}
+		logger.info("exiting getBrowserDetails -> Browser Name="+browser);
+        return browser;
+	}
 
 }
