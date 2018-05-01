e95aec7be27c27a6ea80497f150178cb4242ceec
madem yeswanth babu <yeswanth.madem@kp.org>
2018-05-01 05:54:13 -0700

    US30199: Activity Report: Review All Action to Verify Logs

--- src/org/kp/tpmg/ttg/webcare/videovisits/member/web/utils/WebUtil.java
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/utils/WebUtil.java
@@ -35,7 +35,7 @@ public class WebUtil {
 	public static final String VIDYO_WEBRTC_SESSION_MANGER = "webrtc.health.vidyoworks.com";
 	public static final String LOG_ENTERED = "Entered";
 	public static final String LOG_EXITING = "Exiting";
-	public static final String VV_MBR_BACK_BUTTON = "vv-mbr-back-button";
+	public static final String VV_MBR_BACK_BUTTON = "vv-mbr-back-btn";
 	
 	public static String getCurrentDateTimeZone() {
 		logger.info(LOG_ENTERED);
