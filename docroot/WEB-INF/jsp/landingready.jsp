0278797ccad8259460afc6511fd008ada19d895c
cmohanarangakranthikumar <kranthi.cheluva@kp.org>
2018-06-13 05:02:27 -0700

    Kranti: US31767 and US31769 changes for patient

--- docroot/WEB-INF/jsp/landingready.jsp
+++ docroot/WEB-INF/jsp/landingready.jsp
@@ -7,7 +7,13 @@
 <%@ page import="javax.servlet.*" %>
 <%@ page import="javax.servlet.http.*" %>
 
-<input type="hidden" id="kpKeepAliveUrl" value="${WebAppContext.kpKeepAliveUrl}" /> 
+<input type="hidden" id="kpKeepAliveUrl" value="${WebAppContext.kpKeepAliveUrl}" />
+<!--US31767-->
+<input type="hidden" id="isNonMember" value="${WebAppContext.isNonMember()}" />
+<input type="hidden" id="memberDOmrn" value="${WebAppContext.memberDO.mrn}" />
+<input type="hidden" id="memberDOlastName" value="${WebAppContext.memberDO.lastName}" />
+<input type="hidden" id="memberDOfirstName" value="${WebAppContext.memberDO.firstName}" />
+<!--US31767-->
 <%
 
 	MeetingCommand.retrieveActiveMeetingsForMemberAndProxies(request);
@@ -18,143 +24,23 @@
 <div class="sub-banner">
   <div class="heading">Your Video Visits for Today</div>
   <div class="links">
-    <a href="javascript:location.reload()">Refresh page</a>
+  <!--US31767-->
+    <a href="javascript:getMemberMeetings()">Refresh page</a>
+  <!--US31767-->
     <span>|</span>
     <a href="mdohelp.htm" target="_blank">Help</a>
   </div>
 </div>
-
-<c:if test="${WebAppContext.totalmeetings>0}">
-  <div id="landing-portal-ready" style="width:90%; margin-top:0px; font-family:Avenir Next, sans-serif;">
-  <c:forEach var="meeting" items="${WebAppContext.myMeetings}">
-      <div class="landing-portal-single-container">
-        <c:if test="${meeting.isRunningLate == true}">
-          <!--DE9375 Fix-->
-          <!-- Kranti US19234 start -->
-            <div class="running-late-indicator">We're sorry, your doctor is running late. New start time is <span style="font-size:20px;font-weight: 500;" class="running-late-timestamp-${meeting.meetingId}">${meeting.runLateMeetingTime}</span>.</div>
-          <!-- Kranti US19234 end -->
-            <script type="text/javascript">
-              var cls = ".running-late-timestamp-"+${meeting.meetingId};
-              var meetingTimestamp = $(cls).text();
-              var convertedTimestamp = convertTimestampToDate(meetingTimestamp, 'time_only');
-              $(cls).text(convertedTimestamp);
-            </script>
-          </c:if>
-          <div class="meeting-details-container" style="font-size:14px;">
-            <div class="left">
-              <div class="time-display">
-                <span class="hidden timestamp">${meeting.meetingTime} </span>
-                <span></span>
-              </div>
-              <span>${meeting.member.lastName}, ${meeting.member.firstName} ${meeting.member.middleName}</span>
-              <div class="accord-contents" style="display:block;margin-top:30px;">
-                  <c:if test="${meeting.participant != null && fn:length(meeting.participant) > 0 || meeting.caregiver != null && fn:length(meeting.caregiver) > 0}">
-                    <h2 class="label" style="float:none;margin-bottom:10px;">Additional Participants</h2>
-                  </c:if>
-                  <c:if test="${meeting.participant != null && fn:length(meeting.participant) > 0}">
-                    <div class="names-container-member" style="margin:0px;">
-                      <span class="names participants" style="margin-left:0;">
-                        <c:forEach var="p" items="${meeting.participant}">
-                          <span>${p.firstName.toLowerCase()} ${p.lastName.toLowerCase()}<c:if test="${not empty p.title}">, ${p.title}</c:if></span>
-                        </c:forEach>
-                      </span>
-                    </div>
-                  </c:if>
-                  <div class="names-container-member" style="margin:0px;">
-                    <c:if test="${meeting.caregiver != null && fn:length(meeting.caregiver) > 0}">
-                      <span class="names patient-guests" style="margin-left:0;">
-                        <c:forEach var="p" items="${meeting.caregiver}">
-                          <span>${p.firstName.toLowerCase()} ${p.lastName.toLowerCase()}</span>
-                        </c:forEach>
-                      </span>
-                    </c:if>
-                  </div>
-              </div>
-            </div>
-            <div class="center">
-              <img class="circle-image" src=${meeting.host.imageUrl} alt="" />
-              <!-- start: DE8948: If the clinician does not have title then the name appears with comma at the end in member application.-->
-              <span class="name-and-details camel-case">${meeting.host.firstName} ${meeting.host.lastName}<c:if test="${not empty meeting.host.title}">, ${meeting.host.title}</c:if></span>
-              <!-- end: DE8948: If the clinician does not have title then the name appears with comma at the end in member application.-->
-              <span class="department-details camel-case">${meeting.host.departmentName}</span>
-            </div>
-            <div class="right">
-              <c:choose>
-                <c:when test="${not WebAppContext.isNonMember()}">
-                   <c:choose>
-                      <c:when test="${meeting.meetingVendorId == null || fn:length(meeting.meetingVendorId) <= 0}">
-                        <div style="">
-                          <p style="">
-                              <button class="btn not-available" href="javascript:location.reload()" style="margin-bottom:0;">Join your visit</button> 
-                          </p>
-                          <p class="" style="margin-top:20px;">Your visit will be available within 15 minutes of the start time.</p>
-                        </div>
-                        </c:when>
-                          <c:otherwise>
-                            <div style="">
-                                <c:choose>
-                                  <c:when test="${WebAppContext.memberDO.mrn == meeting.member.mrn}">
-                                    <p class="">
-                                      <button id="joinNowId" class="btn joinNowButton" userName="${WebAppContext.memberDO.lastName}, ${WebAppContext.memberDO.firstName}" meetingid="${meeting.meetingId}" isproxymeeting="N" href="#" style="margin-bottom:0;">Join your visit</button> 
-                                    </p>
-                                    <p class="" style="margin-top:20px;">You may be joining before your clinician. Please be patient.</p>
-                                  </c:when>
-                                  <c:otherwise>
-                                     <p style="">
-                                        <button id="joinNowId" class="btn joinNowButton" userName="${WebAppContext.memberDO.lastName}, ${WebAppContext.memberDO.firstName}, (dummy@dummy.com)" meetingid="${meeting.meetingId}" isproxymeeting="Y" href="#" style="margin-bottom:0;">Join your visit</button> 
-                                    </p>
-                                    <p class="" style="margin-top:20px;">You may be joining before your clinician. Please be patient.</p>
-                                  </c:otherwise>
-                                </c:choose>
-                         </div>
-                   </c:otherwise>
-                  </c:choose>                                   
-               </c:when>
-                <c:otherwise>
-                   <c:choose>
-                        <c:when test="${meeting.meetingVendorId == null || fn:length(meeting.meetingVendorId) <= 0}">
-                          <div style="">
-                            <p style="">
-                                <button class="btn not-available" href="javascript:location.reload()" style="margin-bottom:0;">Join your visit</button> 
-                            </p>
-                            <p class="" style="margin-top:20px;">Your visit will be available within 15 minutes of the start time.</p>
-                          </div>
-                        </c:when>
-                        <c:otherwise>
-                          <div style="">
-                            <p style="">
-                                <button id="joinNowId" class="btn joinNowButton" userName="${WebAppContext.memberDO.lastName}, ${WebAppContext.memberDO.firstName}, (dummy@dummy.com)" meetingid="${meeting.meetingId}" isproxymeeting="Y" href="#" style="margin-bottom:0;">Join your visit</button> 
-                            </p>
-                            <p class="" style="margin-top:20px;">You may be joining before your clinician. Please be patient.</p>
-                          </div>
-                        </c:otherwise>
-                     </c:choose>                      
-                 </c:otherwise>
-            </c:choose>
-            </div>
-            <p class="error error-guest-login" id="error_label_${meeting.meetingId}" style="margin-bottom:25px; font-size:16px;"></p>
-          </div>
-      </div>
-
-     <script type="text/javascript">
-        if(${meeting.participant != null} || ${meeting.caregiver != null}){
-            $('#${meeting.meetingId}').find($(".accord-ctrl-container")).css("display", "inline-block");
-        } else{
-            $('#${meeting.meetingId}').find($(".accord-ctrl-container")).css("display", "none");
-        }
-      </script>
-
-  </c:forEach>
+<!--US31767-->
+<div class="my-meetings-no-meetings-grid my-meets-grid">
+  <div id="landing-portal-ready" class="my-meetings-grid" style="width:90%; margin-top:0px; font-family:Avenir Next, sans-serif;">
+  
   </div>
-</c:if>
-
-<c:if test="${WebAppContext.totalmeetings<=0}">
-
-  <div id="landing-portal-none">
-      <p>You have no meetings in the next 15 minutes.</p>
+  <div id="landing-portal-none" class="no-meetings-grid display-none-cls">
+        <p>You have no meetings in the next 15 minutes.</p>
   </div>
+</div>
 
-</c:if>
 <div id="user-in-meeting-modal"  title="User Already Present In Meeting">
   <div  class="modalWrapper">
     <h2><span>User already signed on</span></h2>
--- docroot/js/site/landingready/landingready.js
+++ docroot/js/site/landingready/landingready.js
@@ -1,10 +1,9 @@
+//US31767
+var getMeetingsTimeoutVal;
+//US31767
 $(document).ready(function() {
     var meetingTimestamp,convertedTimestamp,meetingIdData,hreflocation;
-    /*Satish US14366 - Automatically refresh page every one minute --Start*/
-    setTimeout(function(){window.location.reload();},180000);
-    /*Satish US14366 - Automatically refresh page every one minute --End*/
     initializeUserPresentInMeetingModal();
-    
     //make ajax call to KP Keep alive url
     var keepAliveUrl = $("#kpKeepAliveUrl").val();
     if(keepAliveUrl != null && keepAliveUrl != "" && keepAliveUrl.length > 0 && keepAliveUrl != "undefined")
@@ -31,7 +30,7 @@ $(document).ready(function() {
     }
     
 	// Join now Click Event
-    $(".joinNowButton").click(function(){
+    $(document).delegate('.joinNowButton', 'click', function(){
     	$("#layover").show();
     	//US30802
     	setPeripheralsFlag("true");
@@ -231,7 +230,10 @@ $(document).ready(function() {
             $(this).parent($('.host-name-container')).next($('.accord-contents')).toggle();
             $(this).find($(".accord-ctrl-caret")).removeClass("accord-ctrl-caret-less");
         }
-    });    
+    });
+    //US31767
+    getMemberMeetings();
+    //US31767
 });
 
 function initializeUserPresentInMeetingModal(){	
@@ -271,3 +273,125 @@ function setPeripheralsFlag(flagVal){
     });
 }
 //US30802
+
+
+//US31767
+function getMemberMeetings(){
+    $("#layover").show();
+    window.scrollTo(0, 0);
+    clearTimeout(getMeetingsTimeoutVal);
+    getMeetingsTimeoutVal = setTimeout(function(){getMemberMeetings();},180000);
+	$.ajax({
+        type: "GET",
+        url: VIDEO_VISITS.Path.landingready.dataMemberMeetings,
+        cache: false,
+        dataType: "json",
+        success: function(result){
+            updateDomWithMeetingsData(result);
+        },
+        error: function(error){
+        	console.log(error);
+        },
+        complete: function(){
+            $("#layover").hide();
+        }
+    });
+}
+
+function updateDomWithMeetingsData(data){
+    if(!data.envelope.meetings || data.envelope.meetings.length == 0){
+        $('.my-meetings-grid').css('display', 'none');
+        $('.no-meetings-grid').css('display', 'block');
+    }else{
+        $('.no-meetings-grid').css('display', 'none');
+        $('.my-meetings-grid').css('display', 'block');
+        $('.my-meetings-grid').empty();
+        var htmlToBeAppend = '';
+        for(var i=0;i<data.envelope.meetings.length;i++){
+            var meeting = data.envelope.meetings[i];
+            htmlToBeAppend += '<div class="landing-portal-single-container">';
+            if(meeting.isRunningLate == true || meeting.isRunningLate == "true"){
+                var updatedTime = convertTimestampToDate(meeting.runLateMeetingTime, 'time_only');
+                htmlToBeAppend += '<div class="running-late-indicator">We\'re sorry, your doctor is running late. New start time is <span style="font-size:20px;font-weight: 500;" class="running-late-timestamp-'+meeting.meetingId+'">'+updatedTime+'</span>.</div>';
+            }
+            htmlToBeAppend += '<div class="meeting-details-container" style="font-size:14px;">'
+            htmlToBeAppend += '<div class="left">';
+            var updatedTime = convertTimestampToDate(meeting.meetingTime, 'time_only');
+            htmlToBeAppend += '<div class="time-display"><span class="hidden timestamp">'+meeting.meetingTime+' </span><span>'+updatedTime+'</span></div>';
+            htmlToBeAppend += '<span>'+meeting.member.lastName+', '+meeting.member.firstName+' '+meeting.member.middleName+'</span>';
+            htmlToBeAppend += '<div class="accord-contents" style="display:block;margin-top:30px;">';
+            if(meeting.participant != null && meeting.participant.length > 0 || meeting.caregiver != null && meeting.caregiver.length > 0){
+                htmlToBeAppend += '<h2 class="label" style="float:none;margin-bottom:10px;">Additional Participants</h2>';
+            }
+            if(meeting.participant != null && meeting.participant.length > 0){
+                htmlToBeAppend += '<div class="names-container-member" style="margin:0px;"><span class="names participants" style="margin-left:0;">';
+                for(var j=0;j<meeting.participant.length;j++){
+                    htmlToBeAppend += '<span>'+meeting.participant[j].firstName.toLowerCase()+' '+meeting.participant[j].lastName.toLowerCase();
+                    if(meeting.participant[j].title){
+                        htmlToBeAppend += ', '+meeting.participant[j].title;
+                    }
+                    htmlToBeAppend += '</span>';
+                }
+                htmlToBeAppend += '</span></div>';//.names-container-member class end
+            }
+            htmlToBeAppend += '<div class="names-container-member" style="margin:0px;">';
+            if(meeting.caregiver != null && meeting.caregiver.length > 0){
+                htmlToBeAppend += '<span class="names patient-guests" style="margin-left:0;">';
+                for(var j=0;j<meeting.caregiver.length;j++){
+                    htmlToBeAppend += '<span>'+meeting.caregiver[j].firstName.toLowerCase()+' '+meeting.caregiver[j].lastName.toLowerCase()+'</span>';
+                }
+                htmlToBeAppend += '</span>';
+            }
+            htmlToBeAppend += '</div>';//.names-container-member class end
+            htmlToBeAppend += '</div>';//.accord-contents class end
+            htmlToBeAppend += '</div>';//.left class end
+            htmlToBeAppend += '<div class="center">';
+            htmlToBeAppend += '<img class="circle-image" src='+meeting.host.imageUrl+' alt="" />';
+            htmlToBeAppend += '<span class="name-and-details camel-case"><span style="text-transform:capitalize">'+meeting.host.firstName.toLowerCase()+' '+meeting.host.lastName.toLowerCase()+'</span>';
+            if(meeting.host.title){
+                htmlToBeAppend += ', '+meeting.host.title;
+            }
+            htmlToBeAppend += '</span>';
+            htmlToBeAppend += '<span class="department-details camel-case">'+meeting.host.departmentName.toLowerCase()+'</span>';
+            htmlToBeAppend += '</div>';//.center class end
+            htmlToBeAppend += '<div class="right">';
+            if($('#isNonMember').val() == true || $('#isNonMember').val() == 'true'){
+                //${WebAppContext.isNonMember()}
+                if(meeting.meetingVendorId == null || meeting.meetingVendorId.length <= 0){
+                    htmlToBeAppend += '<div style=""><p style=""><button class="btn not-available" href="javascript:location.reload()" style="margin-bottom:0;">Join your visit</button></p><p class="" style="margin-top:20px;">Your visit will be available within 15 minutes of the start time.</p></div>';
+                }else{
+                    htmlToBeAppend += '<div style=""><p style=""><button id="joinNowId" class="btn joinNowButton" userName="'+$('#memberDOlastName').val()+', '+$('#memberDOfirstName').val()+', (dummy@dummy.com)" meetingid="'+meeting.meetingId+'" isproxymeeting="Y" href="#" style="margin-bottom:0;">Join your visit</button></p><p class="" style="margin-top:20px;">You may be joining before your clinician. Please be patient.</p></div>';
+                }
+
+            }else{
+                //${not WebAppContext.isNonMember()}
+                if(meeting.meetingVendorId == null || meeting.meetingVendorId.length <= 0){
+                    htmlToBeAppend += '<div style=""><p style=""><button class="btn not-available" href="javascript:location.reload()" style="margin-bottom:0;">Join your visit</button></p><p class="" style="margin-top:20px;">Your visit will be available within 15 minutes of the start time.</p></div>';
+                }else{
+                    htmlToBeAppend += '<div style="">';
+                    if($('#memberDOmrn').val() == meeting.member.mrn){
+                        htmlToBeAppend += '<p class=""><button id="joinNowId" class="btn joinNowButton" userName="'+$('#memberDOlastName').val()+', '+$('#memberDOfirstName').val()+'" meetingid="'+meeting.meetingId+'" isproxymeeting="N" href="#" style="margin-bottom:0;">Join your visit</button></p><p class="" style="margin-top:20px;">You may be joining before your clinician. Please be patient.</p>';
+                    }else{
+                        htmlToBeAppend += '<p style=""><button id="joinNowId" class="btn joinNowButton" userName="'+$('#memberDOlastName').val()+', '+$('#memberDOfirstName').val()+', (dummy@dummy.com)" meetingid="'+meeting.meetingId+'" isproxymeeting="Y" href="#" style="margin-bottom:0;">Join your visit</button></p><p class="" style="margin-top:20px;">You may be joining before your clinician. Please be patient.</p>';
+                    }
+                    htmlToBeAppend += '</div>';
+                }
+            }
+            htmlToBeAppend += '</div>';//.right class end
+            htmlToBeAppend += '<p class="error error-guest-login" id="error_label_'+meeting.meetingId+'" style="margin-bottom:25px; font-size:16px;"></p>';
+            htmlToBeAppend += '</div>';//.meeting-details-container class end
+            htmlToBeAppend += '</div>';//.landing-portal-single-container class end
+        }
+        $('.my-meetings-grid').html(htmlToBeAppend);
+        window.setTimeout(function () {
+            for(var x=0;x<data.envelope.meetings.length;x++){
+                if((data.envelope.meetings[x].participant != null && data.envelope.meetings[x].participant.length>0) || (data.envelope.meetings[x].caregiver != null && data.envelope.meetings[x].caregiver.length > 0)){
+                    $('#'+data.envelope.meetings[x].meetingId).find($(".accord-ctrl-container")).css("display", "inline-block");
+                } else{
+                    $('#'+data.envelope.meetings[x].meetingId).find($(".accord-ctrl-container")).css("display", "none");
+                }
+            }
+        },100);
+    }
+}
+//US31767
