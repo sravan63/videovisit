3a9f8c8da283b4a7182406f5a9745d1c81232f7a
umamaheshwararao marripudi <umamaheshwar.marripudi@kp.org>
2019-02-26 02:32:57 -0800

    US37893 : Pexip: Mobile Controller: Member Direct Launch from MDO App : Added Token Validation logic and code re-factored

---
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/jwt/util/JwtUtil.java
@@ -0,0 +1,43 @@
+package org.kp.tpmg.ttg.webcare.videovisits.member.web.jwt.util;
+
+import static org.kp.tpmg.ttg.webcare.videovisits.member.web.utils.WebUtil.LOG_ENTERED;
+import static org.kp.tpmg.ttg.webcare.videovisits.member.web.utils.WebUtil.LOG_EXITING;
+
+import org.apache.log4j.Logger;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+
+public class JwtUtil {
+	public final static String JWT_SECRET = "videovisit-jwt-secret-kaiser-ttg-tpmg-ncal-this-key";
+	public final static long JWT_TOKEN_EXPIRATION_MILLIS = 1800000;//expiration set to 30 mins
+	public static final Logger logger = Logger.getLogger(JwtUtil.class);
+	
+	public static Claims createClaims(String userName){
+		return Jwts.claims().setSubject(userName);
+	}
+	
+	public static String generateJwtToken(final String name){
+		Claims claims = JwtUtil.createClaims(name);
+		return JwtTokenGenerator.generateToken(claims, JWT_SECRET,JWT_TOKEN_EXPIRATION_MILLIS);
+	}
+	
+	public static boolean validateAuthToken(final String authToken) {
+		logger.info(LOG_ENTERED);
+		boolean isValid = false;
+		Claims claims = null;
+		try {
+			claims = JwtTokenValidator.parseToken(authToken, JwtUtil.JWT_SECRET);
+		} catch (Exception e) {
+			logger.error("error while validating token", e);
+		}
+		logger.info(claims);
+		if (claims != null && !claims.isEmpty()) {
+			isValid = true;
+		}
+		logger.info(LOG_EXITING);
+		return isValid;
+	}
+	
+}
