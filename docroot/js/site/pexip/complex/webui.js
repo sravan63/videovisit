a0fc3c49949449da184ce2321723ac0fec9d3d12
Mandar Ashtekar <mandar.ashtekar@gmail.com>
2019-04-29 14:36:58 -0700

    Commenting the TURN Server code

--- docroot/js/site/pexip/complex/webui.js
+++ docroot/js/site/pexip/complex/webui.js
@@ -831,11 +831,11 @@ function initialise(confnode, conf, userbw, username, userpin, req_source, flash
     console.log("Conference: " + conference);
 
     startTime = new Date();
-    rtc.turnServer = {
+    /*rtc.turnServer = {
         url: sidePaneMeetingDetails.vendorConfig.turnServers,
         username: sidePaneMeetingDetails.vendorConfig.turnUserName,
         credential: sidePaneMeetingDetails.vendorConfig.turnPassword
-    };
+    };*/
     rtc.makeCall(confnode, conference, name, bandwidth, source, flash);
 }
 
