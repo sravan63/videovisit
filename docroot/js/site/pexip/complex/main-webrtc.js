b9b0427a42be6f1c9d4e617f007108ba61281680
Phani Bakshi <phani.bakshi@gmail.com>
2019-05-06 05:10:37 -0700

    removed unwanted code

--- docroot/js/site/pexip/complex/main-webrtc.js
+++ docroot/js/site/pexip/complex/main-webrtc.js
@@ -22,38 +22,16 @@ var mobileVideoSources = [];
 //audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
 
 function gotDevices(deviceInfos) {
-  // Handles being called several times to update labels. Preserve values.
-  // var values = selectors.map(select => select.value);
-  // selectors.forEach(select => {
-  //   while (select.firstChild) {
-  //     select.removeChild(select.firstChild);
-  //   }
-  // });
+  
   for (var  i = 0; i < deviceInfos.length; i++) {
     var deviceInfo = deviceInfos[i];
     var option = document.createElement('option');
     option.value = deviceInfo.deviceId;
-    if (deviceInfo.kind === 'audioinput') {
-      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
-      // audioInputSelect.appendChild(option);
-    } else if (deviceInfo.kind === 'audiooutput') {
-      option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
-      // audioOutputSelect.appendChild(option);
-    } else if (deviceInfo.kind === 'videoinput') {
-      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
-      // videoSelect.appendChild(option);
+    if (deviceInfo.kind === 'videoinput') {
       mobileVideoSources.push(option.value);
-    } else {
-      console.log('Some other kind of source/device: ', deviceInfo);
-    }
+    } 
   }
-  // selectors.forEach((select, selectorIndex) => {
-  //   if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
-  //     select.value = values[selectorIndex];
-  //   }
-  // });
-
-  // start();
+  
   configurePexipVideoProperties();
 }
 
