04b8f0b84ccf4c0cdb8011a40f5bd3c3a0bd3170
Phani Bakshi <phani.bakshi@gmail.com>
2019-05-08 08:10:06 -0700

    added new pexip v21rc3 in dev. uncommented late check service call.

--- docroot/js/site/mobile/mobile-web.js
+++ docroot/js/site/mobile/mobile-web.js
@@ -1488,7 +1488,7 @@ function startPexip() {
 	var isProxyMeeting = $('#isProxyMeeting').val();
 	isMobileDevice = true;
 	initialise(roomUrl, alias, bandwidth, name, "", source);
-	//newStartTimeCheckForOneTime();
+	newStartTimeCheckForOneTime();
 	if(isMember == 'true' || isMember == true){
 		setKPHCConferenceStatus(meetingId, "J", isProxyMeeting, decodeURIComponent($('#guestName').val()));
 	}
@@ -1724,5 +1724,5 @@ var newStartTimeCheck = function(){
 	};
 
 	newStartTimeRecursiveCall = window.setInterval(function(){
-		//newStartTimeCheck();
+		newStartTimeCheck();
     },120000);
