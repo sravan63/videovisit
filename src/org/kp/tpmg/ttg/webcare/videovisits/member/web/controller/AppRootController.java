db1a9d27f30ef2f1ce24e695d74b6f0173e72dbc
umamaheshwararao marripudi <umamaheshwar.marripudi@kp.org>
2019-05-30 05:34:42 -0700

    removed logger added for testing

--- src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/AppRootController.java
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/AppRootController.java
@@ -49,7 +49,6 @@ public class AppRootController extends CommonController {
 			logger.info("Context is not null");
 		}
 		updateWebappContext(ctx);
-		logger.info("View Name : " + getViewName());
 		ModelAndView modelAndView = new ModelAndView(getViewName());
 		getEnvironmentCommand().loadDependencies(modelAndView, getNavigation(), getSubNavigation());
 		logger.info(LOG_EXITING);
