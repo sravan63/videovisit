b8b250287eaffff3b4d3631073d45cc7faeca1f7
M980084:RoshanRoshan-Sai.Pratap@kp.org <Roshan-Sai.Pratap@kp.org>
2019-07-24 05:34:26 -0700

    US42876 : Pexip iOS: Select front facing (selfie) camera always select

--- docroot/js/site/mobile/mobile-web.js
+++ docroot/js/site/mobile/mobile-web.js
@@ -1490,6 +1490,10 @@ function startPexip() {
 	var isProxyMeeting = $('#isProxyMeeting').val();
 	isMobileDevice = true;
 	initialise(roomUrl, alias, bandwidth, name, "", source);
+	var os = getAppOS();
+	if(os == "iOS"){
+		$('#cameraSwitch').css('display','none');
+	}
 	newStartTimeCheckForOneTime();
 	if(isMember == 'true' || isMember == true){
 //		setKPHCConferenceStatus(meetingId, "J", isProxyMeeting, decodeURIComponent($('#guestName').val()));
