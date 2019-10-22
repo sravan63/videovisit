30cf627fcee83137a41256896600d75bb0909843
pavan tallapal <pavan.tallapal@kp.org>
2019-10-22 04:39:20 -0700

    [US44734] : React Arch: Backend integration

--- src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/MobileLoginController.java
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/MobileLoginController.java
@@ -10,6 +10,9 @@ import org.apache.log4j.Logger;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.command.MeetingCommand;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.command.WebAppContextCommand;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.context.WebAppContext;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.jwt.util.JwtUtil;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.utils.WebUtil;
+import org.kp.tpmg.videovisit.model.meeting.VerifyMemberEnvelope;
 import org.springframework.web.servlet.ModelAndView;
 
 public class MobileLoginController extends SimplePageController {
@@ -25,14 +28,18 @@ public class MobileLoginController extends SimplePageController {
 			WebAppContext.setWebAppContext(request, ctx);
 		}
 		String data = null;
+		VerifyMemberEnvelope verifyMemberEnvelope = null;
+		String output = null;
 		try {
-			data = MeetingCommand.verifyMember(request);
-			logger.info("data :" + data);
+			verifyMemberEnvelope = MeetingCommand.verifyMember(request);
+			response.setHeader(WebUtil.AUTH_TOKEN, JwtUtil.generateJwtToken(verifyMemberEnvelope.getMember()));
+			logger.debug("data = " + verifyMemberEnvelope);
+			output = WebUtil.prepareCommonOutputJson("submitlogin", "200", "success", verifyMemberEnvelope);
 		} catch (Exception e) {
-			logger.error("System Error" + e.getMessage(), e);
+			logger.error("System Error : ", e);
 		}
 		modelAndView.setViewName(JSONMAPPING);
-		modelAndView.addObject("data", data);
+		modelAndView.addObject("data", output);
 		logger.info(LOG_EXITING);
 		return modelAndView;
 	}
