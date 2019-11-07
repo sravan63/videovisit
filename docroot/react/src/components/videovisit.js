2e901b499c34d277ef730a3a2eeac051ab7013db
Abhishek Kumar Tripurana <tabhishek@pkrmsezdc.com>
2019-11-07 05:59:03 -0800

    Redirect to Home Page

--- docroot/react/src/components/videovisit.js
+++ docroot/react/src/components/videovisit.js
@@ -8,6 +8,7 @@ class Videovisit extends React.Component {
         this.state = { userDetails: {} };
         this.getUserDetails = this.getUserDetails.bind(this);
         //setTimeout(this.getUserDetails.bind(this), 100);
+        setTimeout(this.redirectToSsoLoginPage.bind(this), 100);
         
     }
     redirectToSsoLoginPage() {
