5365401ee9f76ac8270b7800bcccf27ee86edea3
China Babu Chippada <cchina@pkrmsezdc.com>
2019-11-19 04:08:40 -0800

    US45939 ssologin ajax call code changes for member

--- docroot/react/src/components/tempaccess/tempaccess.js
+++ docroot/react/src/components/tempaccess/tempaccess.js
@@ -13,19 +13,24 @@ class TempAccess extends React.Component {
     }
     signOn() {
         localStorage.clear();
-        axios.post('/videovisit/submitlogin.json?last_name=' + this.state.last_name + '&mrn=' + this.state.mrn + '&birth_month=' + this.state.birth_month + '&birth_year=' + this.state.birth_year, {}).then((response) => {
-            if (response && response.status && response.status == 200) {
-                //var data = response.data.data.member;
+        axios.post('/videovisit/submitlogin.json?last_name=' + this.state.lastname + '&mrn=' + this.state.mrn + '&birth_month=' + this.state.birth_month + '&birth_year=' + this.state.birth_year, {}).then((response) => {
+            if (response && response.statusCode == 200) {
+               // if(response.data != "" || response.data != null){
+                // let data = response.data.member;
+                // let fullname = data.firstName + +data.lastName;                
                 //data.isTempAccess = true;
                 //data.ssoSession = response.headers.authtoken;
-                //localStorage.setItem('userDetails', JSON.stringify(data));
+                //localStorage.setItem('userDetails', fullname);
                 this.props.history.push('/myMeetings');
+                // }else{
+                //     this.props.history.push('/myMeetings');
+                // }
             }
         }, (err) => {            
             this.setState({
                 errormsgs :{errorlogin : true,errormsg : "We could not find this patient. Please try entering the information again."} 
             });
-            console.log(this.state);
+            //console.log(this.state);
         });
     }
     handleChange(key, event) {
