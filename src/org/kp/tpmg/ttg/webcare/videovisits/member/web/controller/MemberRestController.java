dfc80caced0041e0d42469191d1fab514ad8a194
mpuneeth <mpuneeth@mpuneeth-lp.pkrmsezdc.com>
2019-12-31 01:36:15 -0800

    US46990:React: Member Join Meeting (backend support).

--- src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/MemberRestController.java
+++ src/org/kp/tpmg/ttg/webcare/videovisits/member/web/controller/MemberRestController.java
@@ -14,6 +14,7 @@ import org.kp.tpmg.ttg.webcare.videovisits.member.web.jwt.util.JwtUtil;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.utils.WebUtil;
 import org.kp.tpmg.videovisit.model.meeting.VerifyMemberEnvelope;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
