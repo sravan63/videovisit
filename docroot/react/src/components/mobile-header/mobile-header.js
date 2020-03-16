a9fae07c418ebe7e160a3b5a0509ffa8887a5d72
Phani Bakshi <phani.bakshi@gmail.com>
2020-03-16 05:13:56 -0700

    Removed unnecessary code from mobile header component.

--- docroot/react/src/components/mobile-header/mobile-header.js
+++ docroot/react/src/components/mobile-header/mobile-header.js
@@ -18,22 +18,6 @@ class MobileHeader extends React.Component {
     }
 
     componentDidMount() {
-        this.subscription = MessageService.getMessage().subscribe((notification) => {
-            switch (notification.text) {
-                case GlobalConfig.NOTIFY_USER:
-                    if (this.loggedInUserName.toLowerCase() !== notification.data.name.toLowerCase()) {
-                        this.setState({ message: notification.data.message, showNotifier: true });
-                        setTimeout(() => {
-                            this.setState({ showNotifier: false });
-                        }, 2500);
-                    }
-                    break;
-
-                case GlobalConfig.ACCESS_MEMBER_NAME:
-                    this.loggedInUserName = JSON.parse(localStorage.getItem('memberName'));
-                    break;
-            }
-        });
         var isMobile = Utilities.isMobileDevice();
         var isInAppAccess = Utilities.getInAppAccessFlag();
         if (isMobile & !isInAppAccess) {
