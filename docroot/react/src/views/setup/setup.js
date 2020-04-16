ba7b396f7a1bf09792c2cd83d6addfeef77937df
Pavan Kumar Tallapalli <pavan.tallapal@kp.org>
2020-04-16 03:28:05 -0700

    Revert "DE19986: IOS Blank page for setup page in Firefox/Chrome browser"
    This reverts commit c460c5c6594cf107102202f9a5586df1a7a1b653

--- docroot/react/src/views/setup/setup.js
+++ docroot/react/src/views/setup/setup.js
@@ -33,11 +33,6 @@ class Setup extends React.Component {
     }
 
     componentDidMount() {
-        var nonSafari = /CriOS|FxiOS/.test(navigator.userAgent);
-        if(nonSafari){
-          this.setState({ isBrowserBlockError: true });
-          return false;
-        }
         MediaService.loadDeviceMediaData();
         this.subscription = MessageService.getMessage().subscribe((message, data) => {
             switch (message.text) {
