9cb5c60ae5168c96ec34a17786e8dc770f666d4e
China Babu Chippada <cchina@pkrmsezdc.com>
2020-06-29 09:20:14 -0700

    Ipad media queries changes applied + footer US

--- docroot/react/src/components/footer/footer.js
+++ docroot/react/src/components/footer/footer.js
@@ -4,7 +4,7 @@ class footer extends Component {
   render() {
     return <div className="container-fluid main-footer">
     <div className="row mt-1 ml-0 mt-2">
-      <div className="col-12 text-center">
+      <div className="col-12 text-center p-0">
         <ul id="list-conditions" className="text-left pl-0">
             <li><a href="http://mydoctor.kaiserpermanente.org/ncal/mdo/terms_and_conditions.jsp" target="_blank" className="pr-2 pb-0"> Terms &amp; Conditions</a></li>
             <li className="last b-0"><a href="https://members.kaiserpermanente.org/kpweb/privacystate/entrypage.do" target="_blank" className="pr-2 pl-2 pb-0">Privacy Practices</a></li>
