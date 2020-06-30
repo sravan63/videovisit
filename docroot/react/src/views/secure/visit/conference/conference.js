631cf931e4cf3435edc11b7ed44ab82e547f9350
China Babu Chippada <cchina@pkrmsezdc.com>
2020-06-30 01:00:16 -0700

    Mobile URL hide changes for reac member

--- docroot/react/src/views/secure/visit/conference/conference.js
+++ docroot/react/src/views/secure/visit/conference/conference.js
@@ -257,12 +257,14 @@ class Conference extends React.Component {
     fullScreenWindow(){
         var doc = window.document;
         var docEl = doc.documentElement;
-        alert(docEl);
+        // alert(docEl);
+
         var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
         var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
 
         if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-            requestFullScreen.call(docEl);
+            document.documentElement.requestFullscreen();
+            // requestFullScreen.call(docEl);
         }
         else {
             cancelFullScreen.call(doc);
