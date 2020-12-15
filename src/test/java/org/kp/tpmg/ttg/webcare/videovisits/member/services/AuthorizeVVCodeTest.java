ea02ea4c32d2882cb6cef4f7e8d1cacc80f6625e
vraghav <vraghav@vraghav-lp.pkrmsezdc.com>
2020-12-15 09:20:06 -0800

    Worked on testcases

--- src/test/java/org/kp/tpmg/ttg/webcare/videovisits/member/services/AuthorizeVVCodeTest.java
+++ src/test/java/org/kp/tpmg/ttg/webcare/videovisits/member/services/AuthorizeVVCodeTest.java
@@ -5,9 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.net.URI;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.data.APIToken;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.model.VVResponse;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.service.WebService;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.utils.ServiceUtil;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,8 @@ import org.springframework.http.ResponseEntity;
 import com.google.gson.Gson;
 
 import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
 
 public class AuthorizeVVCodeTest extends AbstractVideoVisitMemberApiTest {
 	final Gson gson = new Gson();
@@ -24,6 +27,18 @@ public class AuthorizeVVCodeTest extends AbstractVideoVisitMemberApiTest {
 	public void AuthorizeVVCodeTest_Success() throws Exception {
 		VVResponse output = new VVResponse();
 
+		final String outputResp = "{ \"service\": { \"name\": \"authorizeVVCode\", \"status\": { \"code\": \"200\", \"message\": \"Success\" }, \"appVersion\": { \"version\": \"4.0.0\", \"instructions\": null }, \"envelope\": { \"meeting\": { \"meetingId\": \"331890\", \"host\": { \"nuid\": \"A444107\", \"firstName\": \"Jan\", \"lastName\": \"WssoG\", \"title\": \"NP\", \"inMeeting\": \"false\", \"isInvitedMa\": null, \"facilityCode\": \"MOD\", \"departmentCode\": \"PSY\", \"resourceId\": \"6602856\", \"role\": null, \"attendDateTime\": null, \"endDateTime\": null, \"inMeetingDisplayName\": \"WssoG, Jan NP\", \"imageUrl\": \"https://www.permanente.net/pmdb/other/no-photo.jpg\", \"homePageUrl\": \"http://www.kp.org/mydoctor/griffin\", \"subDepartmentCode\": \"CPY2\", \"facilityName\": \"MODESTO MEDICAL CENTER\", \"departmentName\": \"PSYCHIATRY\", \"subDepartmentName\": \"Child & Family Psychiatry\", \"vendorRole\": \"host\", \"inMeetingUniqueId\": null }, \"participant\": null, \"caregiver\": null, \"member\": { \"mrn\": \"14404080\", \"lastName\": \"Birdsong\", \"firstName\": \"Alexander\", \"middleName\": null, \"dateOfBirth\": \"837154800000\", \"gender\": \"M\", \"email\": \"GEETHANJALI.GOPALASWAMY@KP.ORG\", \"attendDateTime\": null, \"endDateTime\": null, \"inMeeting\": \"false\", \"inMeetingDisplayName\": \"Birdsong, Alexander\", \"age\": null, \"vendorRole\": \"guest\", \"inMeetingUniqueId\": null }, \"meetingType\": \"provider-to-member\", \"meetingTime\": \"1599453300000\", \"meetingJoinTime\": null, \"meetingExpireTime\": null, \"createTimestamp\": \"1599453027413\", \"lastUpdateTimestamp\": \"1599453028007\", \"schedulerNuid\": \"A444107\", \"showJoinNow\": \"true\", \"meetingStatus\": \"mm_scheduled\", \"meetingVendorId\": \"m.ncal.psy.0.0.098133.331890\", \"parrsApptId\": null, \"roomJoinUrl\": null, \"roomKey\": null, \"inMeeting\": false, \"encounterId\": null, \"hostNuid\": \"A444107\", \"parrsApptType\": null, \"parrsSecureFlag\": null, \"isRunningLate\": false, \"runLateMeetingTime\": null, \"totalRunLateTimeInMin\": null, \"vendor\": \"pexip\", \"vendorHostPin\": \"331890\", \"vendorGuestPin\": \"098133\", \"sipParticipants\": null, \"authToken\": null, \"vendorConfig\": null, \"memberEmail\": \"test@kp.org\", \"parrsFacilityCode\": null, \"parrsDepartmentCode\": null, \"parrsSubDepartmentCode\": null, \"parrsFacilityName\": null, \"parssDepartmentName\": null, \"parssSubDepartmentName\": null, \"memberStatusCode\": null, \"creationNuid\": null, \"lastUpdateDatetime\": null, \"lastUpdateNuid\": null, \"parrsEpicVisitType\": null, \"parrsActivityCode\": null }, \"firstName\": \"Alexander\", \"lastName\": \"Birdsong\", \"userType\": \"patient\" } } }";
+		
+		APIToken apiToken = new APIToken();
+		apiToken.setAccessToken("79e7524f-66ee-32ea-96d9-dfe256b3ecdc");
+		
+		new MockUp<WebService>() {
+			@Mock
+			APIToken getAPIToken() {
+				return apiToken;
+			}
+		};
+		
 		new Expectations() {
 			{
 				request.getSession().getId();
@@ -34,9 +49,8 @@ public class AuthorizeVVCodeTest extends AbstractVideoVisitMemberApiTest {
 
 				restTemplate.postForEntity((URI) any, (HttpEntity) any, String.class);
 				result = new ResponseEntity<String>(
-						"{ \"service\": { \"name\": \"authorizeVVCode\", \"status\": { \"code\": \"200\", \"message\": \"Success\" }, \"appVersion\": { \"version\": \"4.0.0\", \"instructions\": null }, \"envelope\": { \"meeting\": { \"meetingId\": \"331890\", \"host\": { \"nuid\": \"A444107\", \"firstName\": \"Jan\", \"lastName\": \"WssoG\", \"title\": \"NP\", \"inMeeting\": \"false\", \"isInvitedMa\": null, \"facilityCode\": \"MOD\", \"departmentCode\": \"PSY\", \"resourceId\": \"6602856\", \"role\": null, \"attendDateTime\": null, \"endDateTime\": null, \"inMeetingDisplayName\": \"WssoG, Jan NP\", \"imageUrl\": \"https://www.permanente.net/pmdb/other/no-photo.jpg\", \"homePageUrl\": \"http://www.kp.org/mydoctor/griffin\", \"subDepartmentCode\": \"CPY2\", \"facilityName\": \"MODESTO MEDICAL CENTER\", \"departmentName\": \"PSYCHIATRY\", \"subDepartmentName\": \"Child & Family Psychiatry\", \"vendorRole\": \"host\", \"inMeetingUniqueId\": null }, \"participant\": null, \"caregiver\": null, \"member\": { \"mrn\": \"14404080\", \"lastName\": \"Birdsong\", \"firstName\": \"Alexander\", \"middleName\": null, \"dateOfBirth\": \"837154800000\", \"gender\": \"M\", \"email\": \"GEETHANJALI.GOPALASWAMY@KP.ORG\", \"attendDateTime\": null, \"endDateTime\": null, \"inMeeting\": \"false\", \"inMeetingDisplayName\": \"Birdsong, Alexander\", \"age\": null, \"vendorRole\": \"guest\", \"inMeetingUniqueId\": null }, \"meetingType\": \"provider-to-member\", \"meetingTime\": \"1599453300000\", \"meetingJoinTime\": null, \"meetingExpireTime\": null, \"createTimestamp\": \"1599453027413\", \"lastUpdateTimestamp\": \"1599453028007\", \"schedulerNuid\": \"A444107\", \"showJoinNow\": \"true\", \"meetingStatus\": \"mm_scheduled\", \"meetingVendorId\": \"m.ncal.psy.0.0.098133.331890\", \"parrsApptId\": null, \"roomJoinUrl\": null, \"roomKey\": null, \"inMeeting\": false, \"encounterId\": null, \"hostNuid\": \"A444107\", \"parrsApptType\": null, \"parrsSecureFlag\": null, \"isRunningLate\": false, \"runLateMeetingTime\": null, \"totalRunLateTimeInMin\": null, \"vendor\": \"pexip\", \"vendorHostPin\": \"331890\", \"vendorGuestPin\": \"098133\", \"sipParticipants\": null, \"authToken\": null, \"vendorConfig\": null, \"memberEmail\": \"test@kp.org\", \"parrsFacilityCode\": null, \"parrsDepartmentCode\": null, \"parrsSubDepartmentCode\": null, \"parrsFacilityName\": null, \"parssDepartmentName\": null, \"parssSubDepartmentName\": null, \"memberStatusCode\": null, \"creationNuid\": null, \"lastUpdateDatetime\": null, \"lastUpdateNuid\": null, \"parrsEpicVisitType\": null, \"parrsActivityCode\": null }, \"firstName\": \"Alexander\", \"lastName\": \"Birdsong\", \"userType\": \"patient\" } } }",
-						HttpStatus.OK);
-
+						"{ \"service\": { \"name\": \"authorizeVVCode\", \"status\": { \"code\": \"200\", \"message\": \"Success\" }, \"appVersion\": { \"version\": \"4.0.0\", \"instructions\": null }, \"envelope\": { \"meeting\": { \"meetingId\": \"331890\", \"host\": { \"nuid\": \"A444107\", \"firstName\": \"Jan\", \"lastName\": \"WssoG\", \"title\": \"NP\", \"inMeeting\": \"false\", \"isInvitedMa\": null, \"facilityCode\": \"MOD\", \"departmentCode\": \"PSY\", \"resourceId\": \"6602856\", \"role\": null, \"attendDateTime\": null, \"endDateTime\": null, \"inMeetingDisplayName\": \"WssoG, Jan NP\", \"imageUrl\": \"https://www.permanente.net/pmdb/other/no-photo.jpg\", \"homePageUrl\": \"http://www.kp.org/mydoctor/griffin\", \"subDepartmentCode\": \"CPY2\", \"facilityName\": \"MODESTO MEDICAL CENTER\", \"departmentName\": \"PSYCHIATRY\", \"subDepartmentName\": \"Child & Family Psychiatry\", \"vendorRole\": \"host\", \"inMeetingUniqueId\": null }, \"participant\": null, \"caregiver\": null, \"member\": { \"mrn\": \"14404080\", \"lastName\": \"Birdsong\", \"firstName\": \"Alexander\", \"middleName\": null, \"dateOfBirth\": \"837154800000\", \"gender\": \"M\", \"email\": \"GEETHANJALI.GOPALASWAMY@KP.ORG\", \"attendDateTime\": null, \"endDateTime\": null, \"inMeeting\": \"false\", \"inMeetingDisplayName\": \"Birdsong, Alexander\", \"age\": null, \"vendorRole\": \"guest\", \"inMeetingUniqueId\": null }, \"meetingType\": \"provider-to-member\", \"meetingTime\": \"1599453300000\", \"meetingJoinTime\": null, \"meetingExpireTime\": null, \"createTimestamp\": \"1599453027413\", \"lastUpdateTimestamp\": \"1599453028007\", \"schedulerNuid\": \"A444107\", \"showJoinNow\": \"true\", \"meetingStatus\": \"mm_scheduled\", \"meetingVendorId\": \"m.ncal.psy.0.0.098133.331890\", \"parrsApptId\": null, \"roomJoinUrl\": null, \"roomKey\": null, \"inMeeting\": false, \"encounterId\": null, \"hostNuid\": \"A444107\", \"parrsApptType\": null, \"parrsSecureFlag\": null, \"isRunningLate\": false, \"runLateMeetingTime\": null, \"totalRunLateTimeInMin\": null, \"vendor\": \"pexip\", \"vendorHostPin\": \"331890\", \"vendorGuestPin\": \"098133\", \"sipParticipants\": null, \"authToken\": null, \"vendorConfig\": null, \"memberEmail\": \"test@kp.org\", \"parrsFacilityCode\": null, \"parrsDepartmentCode\": null, \"parrsSubDepartmentCode\": null, \"parrsFacilityName\": null, \"parssDepartmentName\": null, \"parssSubDepartmentName\": null, \"memberStatusCode\": null, \"creationNuid\": null, \"lastUpdateDatetime\": null, \"lastUpdateNuid\": null, \"parrsEpicVisitType\": null, \"parrsActivityCode\": null }, \"firstName\": \"Alexander\", \"lastName\": \"Birdsong\", \"userType\": \"patient\" } } }", HttpStatus.OK);
+				
 			}
 		};
 		final String outputStr = memberRestController.authorizeVVCode(request, response);
--- src/test/java/org/kp/tpmg/ttg/webcare/videovisits/member/services/LaunchMeetingForMemberDesktopTest.java
+++ src/test/java/org/kp/tpmg/ttg/webcare/videovisits/member/services/LaunchMeetingForMemberDesktopTest.java
@@ -8,7 +8,9 @@ import java.net.URI;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.data.APIToken;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.model.VVResponse;
+import org.kp.tpmg.ttg.webcare.videovisits.member.web.service.WebService;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.utils.ServiceUtil;
 import org.kp.tpmg.ttg.webcare.videovisits.member.web.utils.WebUtil;
 import org.springframework.http.HttpEntity;
@@ -18,6 +20,8 @@ import org.springframework.http.ResponseEntity;
 import com.google.gson.Gson;
 
 import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
 
 public class LaunchMeetingForMemberDesktopTest extends AbstractVideoVisitMemberApiTest {
 	static String meetingId = null;
@@ -35,6 +39,16 @@ public class LaunchMeetingForMemberDesktopTest extends AbstractVideoVisitMemberA
 	@Test
 	public void LaunchMeetingForMemberDesktopTest_200() throws Exception {
 		VVResponse output = new VVResponse();
+		
+		APIToken apiToken = new APIToken();
+		apiToken.setAccessToken("79e7524f-66ee-32ea-96d9-dfe256b3ecdc");
+		
+		new MockUp<WebService>() {
+			@Mock
+			APIToken getAPIToken() {
+				return apiToken;
+			}
+		};
 
 		new Expectations() {
 			{
@@ -52,7 +66,7 @@ public class LaunchMeetingForMemberDesktopTest extends AbstractVideoVisitMemberA
 
 				restTemplate.postForEntity((URI) any, (HttpEntity) any, String.class);
 				result = new ResponseEntity<String>(
-						"{ \"service\": { \"name\": \"launchMeetingForMemberDesktop\", \"status\": { \"code\": \"200\", \"message\": \"Success\" }, \"appVersion\": { \"version\": \"3.0.0\", \"instructions\": null }, \"launchMeetingEnvelope\": { \"launchMeeting\": { \"meetingId\": \"602962\", \"host\": { \"nuid\": \"A444107\", \"firstName\": \"Jimmy\", \"lastName\": \"Trainersjo\", \"title\": \"MD\", \"inMeeting\": \"false\", \"isInvitedMa\": null, \"facilityCode\": \"SSC\", \"departmentCode\": \"MED\", \"resourceId\": \"3450656\", \"role\": null, \"attendDateTime\": null, \"endDateTime\": null, \"inMeetingDisplayName\": \"Trainersjo, Jimmy MD\", \"imageUrl\": \"https://www.permanente.net/pmdb/other/no-photo.jpg\", \"homePageUrl\": null, \"subDepartmentCode\": \"MD3B\", \"facilityName\": \"SO SACRAMENTO MEDICAL CENTER\", \"departmentName\": \"ADULT AND FAMILY MEDICINE\", \"subDepartmentName\": null, \"vendorRole\": \"host\" }, \"participant\": null, \"caregiver\": null, \"member\": { \"mrn\": \"14404080\", \"lastName\": \"Mama\", \"firstName\": \"Joe\", \"middleName\": \"\", \"dateOfBirth\": \"-7318800000\", \"gender\": \"M\", \"email\": \"TEST@KP.ORG\", \"attendDateTime\": null, \"endDateTime\": null, \"inMeeting\": \"false\", \"inMeetingDisplayName\": \"Mama, Joe\", \"age\": null, \"vendorRole\": \"guest\" }, \"meetingType\": \"provider-to-member\", \"meetingTime\": \"1577962500000\", \"meetingJoinTime\": null, \"meetingExpireTime\": null, \"createTimestamp\": \"1577962521043\", \"lastUpdateTimestamp\": \"1577962521723\", \"schedulerNuid\": \"A444107\", \"showJoinNow\": \"true\", \"meetingStatus\": \"mm_scheduled\", \"meetingVendorId\": \"m.ncal.med.0.0.269206.602962\", \"parrsApptId\": null, \"roomJoinUrl\": \"vve-tpmg-dev.kp.org\", \"roomKey\": null, \"inMeeting\": false, \"encounterId\": null, \"hostNuid\": \"A444107\", \"parrsApptType\": null, \"parrsSecureFlag\": null, \"isRunningLate\": false, \"runLateMeetingTime\": null, \"totalRunLateTimeInMin\": null, \"vendor\": \"pexip\", \"vendorHostPin\": \"602962\", \"vendorGuestPin\": \"269206\", \"sipParticipants\": null, \"authToken\": null, \"vendorConfig\": { \"turnServers\": [\"ivv-turn-dev.kp.org:443\"], \"turnUserName\": \"kpturnuser\", \"turnPassword\": \"LLBASGFnsla2312\" }, \"memberEmail\": \"TEST@KP.ORG\", \"parrsFacilityCode\": null, \"parrsDepartmentCode\": null, \"parrsSubDepartmentCode\": null, \"parrsFacilityName\": null, \"parssDepartmentName\": null, \"parssSubDepartmentName\": null, \"memberStatusCode\": null, \"creationNuid\": null, \"lastUpdateDatetime\": null, \"lastUpdateNuid\": null, \"parrsEpicVisitType\": null, \"parrsActivityCode\": null } } } }",
+						"{ \"service\": { \"name\": \"launchMeetingForMemberDesktop\", \"status\": { \"code\": \"200\", \"message\": \"Success\" }, \"appVersion\": { \"version\": \"3.0.0\", \"instructions\": null }, \"launchMeetingEnvelope\": { \"launchMeeting\": { \"meetingId\": \"602962\", \"host\": { \"nuid\": \"A444107\", \"firstName\": \"Jimmy\", \"lastName\": \"Trainersjo\", \"title\": \"MD\", \"inMeeting\": \"false\", \"isInvitedMa\": null, \"facilityCode\": \"SSC\", \"departmentCode\": \"MED\", \"resourceId\": \"3450656\", \"role\": null, \"attendDateTime\": null, \"endDateTime\": null, \"inMeetingDisplayName\": \"Trainersjo, Jimmy MD\", \"imageUrl\": \"https://www.permanente.net/pmdb/other/no-photo.jpg\", \"homePageUrl\": null, \"subDepartmentCode\": \"MD3B\", \"facilityName\": \"SO SACRAMENTO MEDICAL CENTER\", \"departmentName\": \"ADULT AND FAMILY MEDICINE\", \"subDepartmentName\": null, \"vendorRole\": \"host\" }, \"participant\": null, \"caregiver\": null, \"member\": { \"mrn\": \"14404080\", \"lastName\": \"Mama\", \"firstName\": \"Joe\", \"middleName\": \"\", \"dateOfBirth\": \"-7318800000\", \"gender\": \"M\", \"email\": \"TEST@KP.ORG\", \"attendDateTime\": null, \"endDateTime\": null, \"inMeeting\": \"false\", \"inMeetingDisplayName\": \"Mama, Joe\", \"age\": null, \"vendorRole\": \"guest\" }, \"meetingType\": \"provider-to-member\", \"meetingTime\": \"1577962500000\", \"meetingJoinTime\": null, \"meetingExpireTime\": null, \"createTimestamp\": \"1577962521043\", \"lastUpdateTimestamp\": \"1577962521723\", \"schedulerNuid\": \"A444107\", \"showJoinNow\": \"true\", \"meetingStatus\": \"mm_scheduled\", \"meetingVendorId\": \"m.ncal.med.0.0.269206.602962\", \"parrsApptId\": null, \"roomJoinUrl\": \"vve-tpmg-dev.kp.org\", \"roomKey\": null, \"inMeeting\": false, \"encounterId\": null, \"hostNuid\": \"A444107\", \"parrsApptType\": null, \"parrsSecureFlag\": null, \"isRunningLate\": false, \"runLateMeetingTime\": null, \"totalRunLateTimeInMin\": null, \"vendor\": \"pexip\", \"vendorHostPin\": \"602962\", \"vendorGuestPin\": \"269206\", \"sipParticipants\": null, \"authToken\": null, \"vendorConfig\": { \"turnServers\": [\"ivv-turn-dev.kp.org:443\"], \"turnUserName\": \"kpturnuser\", \"turnPassword\": \"LLBASGFnsla2312\" }, \"memberEmail\": \"TEST@KP.ORG\", \"parrsFacilityCode\": null, \"parrsDepartmentCode\": null, \"parrsSubDepartmentCode\": null, \"parrsFacilityName\": null, \"parssDepartmentName\": null, \"parssSubDepartmentName\": null, \"memberStatusCode\": null, \"creationNuid\": null, \"lastUpdateDatetime\": null, \"lastUpdateNuid\": null, \"parrsEpicVisitType\": null, \"parrsActivityCode\": null } } } }", 
 						HttpStatus.OK);
 
 			}
