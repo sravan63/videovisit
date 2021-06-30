cf347458431e409a6d07d63b57a24c8a8607dfce
DESKTOP-UK7M7J4\kabhishek1 <abhishek.x5.kumar@kp.org>
2021-06-30 10:34:51 +0530

    DE22583 and DE22584: PIP Crop and fit fix

--- docroot/react/src/views/secure/visit/conference/conference.js
+++ docroot/react/src/views/secure/visit/conference/conference.js
@@ -1191,10 +1191,12 @@ class Conference extends React.Component {
                 if(participantCount === 2 && this.screenMode ==='portrait' && isNotAudioCall && !this.state.showSharedContent && isHostAvail) {
                     let vh = window.innerHeight - 50;
                     this.remoteFeedMedia.current.style.setProperty('height', `${vh}px`);
+                    this.remoteFeedMedia.current.style.setProperty('min-height', `${vh}px`); //just to avoid safari iPhone white space bug
                     return true;
                 }
             }
         }
+        this.remoteFeedMedia.current.style.removeProperty("min-height");
         this.remoteFeedMedia.current.style.removeProperty("height");
         return false;
     }
