f7f7786479d6f6bcae85becc5d7b82b530e2bf8d
DESKTOP-UK7M7J4\kabhishek1 <abhishek.x5.kumar@kp.org>
2021-07-01 13:49:23 +0530

    DE22584

--- docroot/react/src/views/secure/visit/conference/conference.js
+++ docroot/react/src/views/secure/visit/conference/conference.js
@@ -1238,6 +1238,10 @@ class Conference extends React.Component {
                 let participantCount = this.state.participants.length;
                 if(participantCount === 2 && this.screenMode ==='portrait' && isNotAudioCall && !this.state.showSharedContent && isHostAvail) {
                     let vh = window.innerHeight - 50;
+                    //To avoid DE22584
+                    if(/iPhone/.test(navigator.userAgent) ) {
+                        vh = document.documentElement.clientHeight -50;
+                    }
                     this.remoteFeedMedia.current.style.setProperty('height', `${vh}px`);
                     return true;
                 }
