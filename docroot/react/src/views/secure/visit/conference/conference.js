08d7ea4cec903f2514050a08ffe7e5918c6cad53
DESKTOP-UK7M7J4\kabhishek1 <abhishek.x5.kumar@kp.org>
2021-07-30 14:17:55 +0530

    DE22775: changes - REVERT

--- docroot/react/src/views/secure/visit/conference/conference.js
+++ docroot/react/src/views/secure/visit/conference/conference.js
@@ -723,13 +723,8 @@ class Conference extends React.Component {
         });
     }
 
-    deviceChanged(e) {
-        let camera = this.state.isRearCamera ? this.state.media["videoinput"][0].label : this.state.media["videoinput"][1].label;
-        
-        //DE22775
-        if(e && e.label!== camera.label) {
-            MediaService.onDeviceChange();
-        }
+    deviceChanged() {
+        MediaService.onDeviceChange();
     }
 
     sendMediaStats(data) {
