1f721ccd3d5cf91a1093e892897dad0df1095003
China Babu Chippada <cchina@pkrmsezdc.com>
2021-08-08 03:13:49 -0700

    DE22775: VV_Member_iPad_Continuously  camera switches when we tap on switch camera button.

--- docroot/react/src/views/secure/visit/conference/conference.js
+++ docroot/react/src/views/secure/visit/conference/conference.js
@@ -724,7 +724,13 @@ class Conference extends React.Component {
     }
 
     deviceChanged() {
-        MediaService.onDeviceChange();
+        let camera = this.state.isRearCamera ? this.state.media["videoinput"][0].label : this.state.media["videoinput"][1].label;
+        
+        //DE22775
+        if(e && e.label!== camera.label) {
+            MediaService.onDeviceChange();
+        }
+
     }
 
     sendMediaStats(data) {
