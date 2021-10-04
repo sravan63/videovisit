55f20a9505fe4a8b079091852b9eada675b94752
Manisha Miriyala <manisha.X.miriyala@kp.org>
2021-10-04 14:50:45 -0700

    Revert "US59345- jnotify displaying phone number"
    This reverts commit 2ac3b736712709b1855cf470e33ef1347941bcdd.

--- docroot/react/src/views/secure/visit/conference/conference.js
+++ docroot/react/src/views/secure/visit/conference/conference.js
@@ -1080,11 +1080,7 @@ class Conference extends React.Component {
         var guestName;
         caregiver.forEach(function(val, index) {
             if (val.careGiverMeetingHash == details.meetingCode) {
-                let emailOrPhone = val.emailAddress ? val.emailAddress : val.phoneNumber;
-                if (emailOrPhone == val.phoneNumber)
-                    guestName = val.lastName + ', ' + val.firstName;
-                else
-                    guestName = val.lastName + ', ' + val.firstName + ', (' + emailOrPhone + ')';
+                guestName = val.lastName + ', ' + val.firstName + ', ' + val.emailAddress ? '(' + val.emailAddress + ')' : '' ;
             }
         });
         return guestName;
