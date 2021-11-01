1c61361573e8b664d3660124ab05876ea066c798
Abhishek kumar Tripurana <abhishekkumar.x.tripurana@kp.org>
2021-11-01 14:03:47 +0530

    US52103 and US52070 : separated header and footer components

--- docroot/react/src/components/email-instructions/guest-instructional/guest-instructional.js
+++ docroot/react/src/components/email-instructions/guest-instructional/guest-instructional.js
@@ -70,111 +70,37 @@ class GuestInstructional extends Component {
     render() {
         let details = this.state.staticData;
         return (
-            <div>
-                <table border="0" cellPadding="0" cellSpacing="0" className="email-container">
-                    <tbody>
-                    <tr>
-                        <td className="container">
-                            <div className="content">
-                                <table className="main-container">
-                                    <tbody>
-                                    <tr>
-                                        <td>
-                                            <table border="0" className="header" cellPadding="0" cellSpacing="0">
-                                                <tbody>
-                                                <tr>
-                                                    <td align="left" rowSpan="2" className="left-header">
-                                                        <a href="https://mydoctor.kaiserpermanente.org/ncal/mdo/#/?origin=sysoutreach"
-                                                           target="_blank">
-
-                                                            <img className="logo-image" border="0" src={PermanenteHeader}
-                                                                 alt="The Permanente Medical Group"/>
-                                                        </a>
-
-                                                    </td>
-                                                    <td align="right"  className="right-header">
-                                                        <a href="https://mydoctor.kaiserpermanente.org/ncal/mdo/#/?origin=sysoutreach"
-                                                           target="_blank">
-                                                            <img
-                                                                src={Logo}
-                                                                border="0" alt="The Permanente Medical Group"
-                                                                className="right-logo"/>
-                                                        </a>
-                                                    </td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="wrapper">
-                                            <table border="0" cellPadding="0" cellSpacing="0">
-                                                <tbody>
-                                                    <tr>
-                                                        <td className="instruction">
-                                                            <p className="instruct-header">{details.heading} </p>
-                                                            <p className="instruct-description">{details.subHeading}</p>
-                                                            <div className="visit-rules">
-                                                                <ul>
-                                                                    <li style={{ fontWeight:'bold' }}>{details.rule1}</li>
-                                                                    <li>{details.rule2}</li>
-                                                                    <li>{details.rule3}</li>
-                                                                    <li style={{marginBottom:'12px'}}>{details.rule4}</li>
-                                                                </ul>
-                                                            </div>
-                                                            <div className="text-center">
-                                                                <a href={this.state.emailContentDetails.meetingURL}
-                                                                   target="_blank" className="start-visit">{details.startVisit}</a>
-                                                            </div>
-                                                            <div className="time-details">
-                                                                <p>{details.dateTime}: {this.getHoursAndMinutes(this.state.emailContentDetails.meetingTimeStamp,'date',this.state.emailContentDetails.lang)} at {this.getHoursAndMinutes(this.state.emailContentDetails.meetingTimeStamp,'time',this.state.emailContentDetails.lang)} Pacific Time</p>
-                                                                <p>{details.patient}: {this.state.emailContentDetails.memberFirstName} {this.state.emailContentDetails.lastNameFirstCharMember}</p>
-                                                                <p>{details.clinician}: {this.state.emailContentDetails.doctorFirstName} {this.state.emailContentDetails.doctorLastName} {this.state.emailContentDetails.doctorTitle}</p>
-                                                            </div>
-                                                            <div className="learn-more">
-                                                            <a href={this.state.emailContentDetails.guestHelpUrl} target="_blank">{details.learnMore}</a>
-                                                            </div>
-                                                            <div className="need-help">
-                                                                <p>{details.needHelp1}</p>
-                                                                <p>{details.needHelp2}</p>
-                                                                <p>{details.needHelp3}</p>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                    <tr className="footer">
-                                        <td className="footer-text">
-                                            <a>
-                                                <img src={FooterLogo} className="footer-logo"/>
-                                            </a>
-                                        </td>
-                                        <td className="content-block">
-                                            <span className="apple-link">{details.notification}</span>
-                                        </td>
-                                        <td className="important-block">
-                                            <span style={{fontWeight:'bold'}}>{details.important}</span>
-                                            <ul style={{marginBottom:'0'}}>
-                                                <li>{details.emergency1}</li>
-                                                <li>{details.emergency2}</li>
-                                            </ul>
-                                        </td>
-                                        <td className="future-announcement">{details.futureAnnouncement1}<a href="#" style={{color: '#92ccf0'}}>{details.doNotReply}</a>{details.futureAnnouncement2}
-                                        </td>
-                                        <td className="privacy">
-                                            <a href="https://healthy.kaiserpermanente.org/privacy" target="_blank" style={{color: '#92ccf0'}}>{details.privacy}</a></td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-
-                            </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>)
+                <div className="wrapper">
+                    <div className="instruction">
+                        <p className="instruct-header">{details.heading} </p>
+                        <p className="instruct-description">{details.subHeading}</p>
+                        <div className="visit-rules">
+                            <ul>
+                                <li style={{ fontWeight:'bold' }}>{details.rule1}</li>
+                                <li>{details.rule2}</li>
+                                <li>{details.rule3}</li>
+                                <li style={{marginBottom:'12px'}}>{details.rule4}</li>
+                            </ul>
+                        </div>
+                        <div className="text-center">
+                            <a href={this.state.emailContentDetails.meetingURL}
+                               target="_blank" className="start-visit">{details.startVisit}</a>
+                        </div>
+                        <div className="time-details">
+                            <p>{details.dateTime}: {this.getHoursAndMinutes(this.state.emailContentDetails.meetingTimeStamp,'date',this.state.emailContentDetails.lang)} at {this.getHoursAndMinutes(this.state.emailContentDetails.meetingTimeStamp,'time',this.state.emailContentDetails.lang)} Pacific Time</p>
+                            <p>{details.patient}: {this.state.emailContentDetails.memberFirstName} {this.state.emailContentDetails.lastNameFirstCharMember}</p>
+                            <p>{details.clinician}: {this.state.emailContentDetails.doctorFirstName} {this.state.emailContentDetails.doctorLastName} {this.state.emailContentDetails.doctorTitle}</p>
+                        </div>
+                        <div className="learn-more">
+                            <a href={this.state.emailContentDetails.guestHelpUrl} target="_blank">{details.learnMore}</a>
+                        </div>
+                        <div className="need-help">
+                            <p>{details.needHelp1}</p>
+                            <p>{details.needHelp2}</p>
+                            <p>{details.needHelp3}</p>
+                        </div>
+                    </div>
+                </div>)
     }
 }
 
