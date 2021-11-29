08a46bd58af38b3fda6001702081951678517a8f
Abhishek kumar Tripurana <abhishekkumar.x.tripurana@kp.org>
2021-11-29 11:27:47 +0530

    test commit...

--- docroot/react/src/services/global.config.js
+++ docroot/react/src/services/global.config.js
@@ -6,7 +6,7 @@ const GlobalConfig = {
     VIDEO_VISIT_ROOM_URL: _rootUrl + 'videoVisitReady',
     LOGIN_URL: _rootUrl + 'login',
     IPHONE:'iPhone',
-    IPHONE_VERSION:'15.1',
+    IPHONE_VERSION:'16.1',
     MEETINGS_URL: _rootUrl + 'myMeetings',
     AUTO_REFRESH_TIMER: '180000',
     RUNNING_LATE_TIMER: 120000,
@@ -545,7 +545,7 @@ const GlobalConfig = {
               "english": "SURGERY",
               "spanish": "CIRUGÍA",
               "chinese": "外科手術部"
-            },            
+            },
             {
               "english": "TRANSPLANT SERVICES",
               "spanish": "SERVICIOS DE TRASPLANTES",
